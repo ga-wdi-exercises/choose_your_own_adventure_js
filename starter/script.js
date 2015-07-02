7ccc29567ebd1a385f8b619a02ca3ab787183e31
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-02 19:11:57 -0400

    completed timeleft under 5 branch

--- starter/script.js
+++ starter/script.js
@@ -17,12 +17,11 @@ if (nFLQB === "y") {
   console.log("Great! Me too. My favorite QB is RG3. So let's pretend you're RG3 for a play.");
   buffer();
   var timeLeft = prompt("It's 3rd down and 7 at your own 35 yard line.\nYou've broken the huddle and walk up to the line.\nBefore you hike the ball, you look at the play clock.\nHow much time is left on the clock?\n Seconds - 1 to 24");
-  //console.log(timeLeft);
   if (timeLeft > 5){
     console.log("Congrats on not taking forever to get to the line.");
     buffer();
     var playCall = prompt("You snap the ball. What would you like to do?\n Handoff\n Pass");
-      if (playCall == "handoff") {
+      if (playCall == "handoff" || "Handoff") {
         console.log("Good idea to hand it off to Alfred.");
         buffer();
         var howManyYards = prompt("It looks like there's a big hole for Alfred to run through.\nHow many yards does he run?\n Please enter a number.");
@@ -67,6 +66,17 @@ if (nFLQB === "y") {
           }
       }
   } else if (timeLeft <= 5){
-    console.log("Time is running out. You should have moved faster.");
+    console.log("You really should have moved faster.");
+    buffer();
+      var callTimeout = prompt("Time is running out. You have two timeouts. Do you call one?\n Yes = y\n No = n");
+        if (callTimeout == "y"){
+          console.log("You have called a timeout. At least it wasn't the worst thing that could have happened.");
+          console.log("Shake your head in disbelief as you head to the sideline.");
+          buffer();
+        } else {
+          console.log("You don't get the ball snapped in time. Delay of game.");
+          console.log("Shake your head in disbelief as you walk an extra 5 yards back to the huddle.");
+          buffer();
+        }
   }
 }
