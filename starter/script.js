4e8d028f43860d1bbb5347a2cfdd9fbdb371c41b
Robert <i.am.robert.benjamin@gmail.com>
2015-07-04 15:40:10 -0400

    Add initial structure for paths

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,26 @@
+console.log("Welcome to Robert's Choose-Your-Own Adventures!");
+console.log("****************************************")
+console.log("The year is 2015, and you just finished the day's GA lessons. As you get up to leave the classroom, you notice a dark shadow slide past the doorway. What do you do?");
+
+var choice1 = prompt("You (c)hase after the shadow, (s)tay put to see if anyone else noticed it, or (j)ump out the window, fearing for your life.");
+
+switch(choice1) {
+	case "c":
+		console.log("You jump out of your seat, stumble over a laptop cord, and spring out of the classroom after knocking over two or three macbooks. You see a glipse of the dark shadow heading towards the elevators. What do you do?")
+		var choice 2 = prompt("")
+	break;
+
+	case "s":
+
+
+	break;
+
+	case "j":
+
+
+	break;
+
+	default:
+		console.log("Make sure to choose 'c', 's', or 'j'.")
+}
 // put code here!
