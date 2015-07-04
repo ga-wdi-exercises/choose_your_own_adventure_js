e8109243f5757d3d0527021e2fe4322ee701b17f
Matt Ficke <matthew.ficke@gmail.com>
2015-07-04 14:28:13 -0400

    adding first alert on page load

--- starter/script.js
+++ starter/script.js
@@ -1,2 +1,44 @@
 // put code here!
 // steal storyline from LPTHW text adventure
+/*Rules
+
+Goal Using what you know about data types, variables, operators,
+conditional blocks, and accepting user input, create a text-based game that
+allows the user to arrive at different "destinations" based on the inputs they
+type.
+
+Any path the user goes down must ask them at least
+3 questions(more if you want!)
+
+There must be a minimum of 7 total destinations the user could arrive at based
+on their responses.(again more if you want!)
+For at least 1(also can be more!) of the questions asked, there must be more
+then 2 possible user responses
+Adventure must contain at least one integer response and one string response
+Adventure must contain at least one functional loop in the code base
+hint you may need parseInt() and toString()*/
+
+//Intro
+//Welcome to the land of sunshine, candy, and unicorns! Careful, though --
+//some parts around here aren't so friendly.
+//'What's your name?''
+// **save name as var**
+function anteRoom() {
+  alert("Welcome to the land of SUNSHINE, CANDY, and UNICORNS! Careful, though -- some parts around here aren\'t so friendly.");
+}
+
+anteRoom();
+
+//'What would you like to see while you're here?''
+//if response is 'sunshine', 'candy', or 'unicorns', direct them to the
+//relevant room.
+
+//If anything else, randomly direct them to "darkness", "hunger", "or "wolves".
+
+//Solve a puzzle in each room. If successful in happy room, progress to next
+//happy room. If in unhappy room, move back to a happy room.
+
+//If unsuccessful in happy room, assign to unhappy room. If in unhappy
+//room, die.
+
+//Final puzzle: sends you wither to happiness castle or pit of despair.
