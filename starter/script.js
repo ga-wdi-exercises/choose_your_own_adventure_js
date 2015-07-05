d18d4db0da4b1c36568e772587a17defc24cac46
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-04 21:57:31 -0400

    remaining item - for loop to display score after each input from end user

--- starter/script.js
+++ starter/script.js
@@ -1,48 +1,43 @@
-Minimum Requirements:
-// (1) 3 questions per path - completed
-// (2) 7 places the user could end up at - completed
-// (3) 1 question must be answered with multiple responses - completed
-// (4) 1 integer response - completed
-// (5) 1 string response - completed
-// (6) 1 loop - completed
-
-// Questions:
-// (1) how do I keep prompting the player to move through the questions? I've got an alert set, but that won't prompt a user for more info. - solved by substituting "prompt" for "alert"
-// (2) how do I properly test this code? repl.it isn\'t prompting a user for input as I expected. - solved by using jfiddle
-// (3) how do I loop through the array that contains the integers pushed into it after each user answer, then return the sum of those integers to reveal an overall score?
+// Question(s):
+// (1) How do I loop through the array that contains the user's answers that were pushed into it after each round, then return the sum of those integers to reveal an overall score? Ideally the total score would be returned after each round of play.
 
 
 var userAnswers = [];
 userAnswers.push(firstChoice, secondChoice, thirdChoice);
-var sumOfAnswers = 0;
-for (var i=0; i>1; i++) {
-  return sumOfAnswers[i];
+
+function totalScore(userAnswers) {
+  var sumOfAnswers = 0;
+  for (var i=0; i<userAnswers.length; i++) {
+    sumOfAnswers += userAnswers[i];
+  }
+  alert("Your score is " + sumOfAnswers + " and as soon I get the code right I'll evaluate whether or not you won");
 }
 
-var letsPlay = prompt("Hello! Are you ready to see what playing Chutes & Ladders is like? Type yes, no, or maybe");
+var letsPlay = prompt("Hello! Are you ready to play Chutes & Ladders? \n (yes/no/maybe)");
 
+// Round 1
  if (letsPlay == "yes" || letsPlay == "maybe") {
    //first user destination
-   var firstChoice = prompt("Let's get started! You will need to type the number of your selection for each question in this game. Would you like to 1. ride a bike 2. bake a cake 3. rescue a kitten from high up in a tree?");
+   var firstChoice = prompt("Let's get started! You will need to type the number of your selection for each question in this game. Would you rather: \n 1. Ride a bike \n 2. Bake a cake \n 3. Rescue a kitten from high up in a tree");
  } else if (letsPlay == "no") {
    //second user destination
-   alert("No hard feelings. See you next time!");
+   alert("See you next time!");
  }
 
+// Round 2
   if (firstChoice == 1) {
    //third user destination
     alert("You crashed! Game over. Go get some band-aids.");
   } else if (firstChoice == 2 || firstChoice == 3) {
     //fourth user destination
-    var secondChoice = prompt("Well done! Would you like to 1. plant flowers 2. read a book 3. steal cookies from the cookie jar?");
+    var secondChoice = prompt("Well done! Would you rather: \n 1. Plant flowers \n 2. Read a book \n 3. Steal cookies from the cookie jar");
   }
 
+// Round 3
  if (secondChoice == 3) {
    //fifth user destination
    alert("You pinched your finger while shutting the jar. Game over! Go get a bag of ice.");
   } else if (secondChoice == 1 || secondChoice == 2) {
     //sixth user destination
-    var thirdChoice = prompt("Great choice! You get a bonus selection for doing so well. Would you like to 1. eat ice cream 2. take a nap 3. run amok? Type your two selections one after the other without spaces (xx)");
+    var thirdChoice = prompt("Great choice! You get a bonus selection for doing so well. Would you rather: \n 1. Eat ice cream \n 2. Take a nap \n 3. Run amok \n Type your two selections one after the other without spaces (xx)");
   }
-
-  alert(sumOfAnswers);
