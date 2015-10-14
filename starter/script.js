63455c0a89b77a1899d15ab0d7c00ab54f2d9cf6
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-14 18:34:25 -0400

    added to adventure in script

--- starter/script.js
+++ starter/script.js
@@ -1,75 +1,155 @@
 // put code here!
 var name = prompt("Welcome to 'Choose your own adventure'! What's your name?")
 
-console.log("Ok " + name + "! Let's find a trip that suits your preferences..");
+console.log("Ok " + name + "! Time to plan a trip..");
 
   var budget = prompt("What is your maximum budget?");
-    if (budget <= 100) {
-      console.log("Ah, " + budget + "? Well we will probably have to stick locally in the city");
-      var hundredTrip = prompt("So, do you like (select letter): a. Sports?" + " b. Museums?" + " c. Theater/arts?");
-        if (hundredTrip === "a") {
-          var hundredSport = prompt("Ok, do you like (select letter): a. Baseball?" + " b. Basketball?" + " c. Hockey?");
-            if( hundredSport === "a") {
+    if (budget <= 300) {
+      console.log("With " + budget + " it looks like you'll have to stick locally in the city");
+      var cheapTrip = prompt("Do you want to see (select letter): a. Sports?" + " b. Museums?" + " c. Theater/arts?");
+        if (cheapTrip === "a") {
+          var cheapSport = prompt("Ok, do you like (select letter): a. Baseball?" + " b. Basketball?" + " c. Hockey?");
+            if( cheapSport === "a") {
               console.log("You've received tickets to the Washington Nationals!")
-            } else if ( hundredSport === "b") {
+            } else if ( cheapSport === "b") {
               console.log("You've received tickets to the Washington Wizards!")
-            } else if ( hundredSport === "c") {
+            } else if ( cheapSport === "c") {
               console.log("You've received tickets to the Washington Capitals!")
             } else {
               console.log("Sorry, that was not an option, please start over");
             };
-        } else if (hundredTrip === "b") {
-          var hundredMuseum = prompt("Ok, do you like (select letter): a. Art?" + " b. Science?" + " c. History?");
-            if( hundredMuseum === "a") {
+        } else if (cheapTrip === "b") {
+          var cheapMuseum = prompt("Ok, do you like (select letter): a. Art?" + " b. Science?" + " c. History?");
+            if( cheapMuseum === "a") {
               console.log("You've been sent to the National Gallery!")
-            } else if ( hundredMuseum === "b") {
+            } else if ( cheapMuseum === "b") {
               console.log("You've been sent to the Air and Space Museum!")
-            } else if ( hundredMuseum === "c") {
+            } else if ( cheapMuseum === "c") {
               console.log("You've been sent to the National Museum of American History!")
             } else {
               console.log("Sorry, that was not an option, please start over");
             };
-        } else if (hundredTrip === "c") {
-          var hundredTheater = prompt("Ok, do you like (select letter): a. Comedy?" + " b. Music?" + " c. Drama?");
-            if( hundredTheater === "a") {
+        } else if (cheapTrip === "c") {
+          var cheapTheater = prompt("Ok, do you like (select letter): a. Comedy?" + " b. Music?" + " c. Drama?");
+            if( cheapTheater === "a") {
               console.log("You've been booked a ticket for Avenue Q the next time it comes to town")
-            } else if ( hundredTheater === "b") {
+            } else if ( cheapTheater === "b") {
               console.log("You've been booked a ticket for The Roots Picnic next June")
-            } else if ( hundredTheater === "c") {
+            } else if ( cheapTheater === "c") {
               console.log("Collect your tickets for Les Mis next time it's in town!")
             } else {
               console.log("Sorry, that was not an option, please start over");
             };
         }
-    }
-
-
-
-
-//     var youngTrip = prompt("Are you looking to (select letter): a. Go abroad?" + " b. Stay in the country?" + " c. Stay in this city?");
-//       if (youngTrip === "a") {
-//         console.log("Fun! Let's send you abroad!");
-//
-//
-//
-//         var youngContinent = prompt("Do you prefer a specific continent (y, n)?");
-//           if (youngContinent === "n") {
-//             console.log("Ok great! Well we are going to send you to Antarctica, because teenagers are the worst and you belong there.")
-//           } else if (youngContinent === "y") {
-//
-//           } else {
-//             console.log("Sorry, please start over and choose 'y' or 'n'");
-//           }
-//       }
-//
-//
-//
-//
-//
-//
-//
-// } else if (age < 21 && age >18) {
-//
-// } else {
-//
-// }
+    } else if (budget > 300 && budget <= 4000) {
+      console.log("With a budget of " + budget + " you get to take a fun trip!");
+      var niceTrip = prompt("Are you ok to go abroad (y/n)?");
+        if (niceTrip === "y") {
+          console.log("You chose to go abroad. The options are Europe, or South America")
+            var niceCont = prompt("Choose your location (select letter)? a. Europe" + " b. South America");
+              if(niceCont === "a") {
+                  console.log("You will be sent to Paris first for the beginning of a 2 week excursion");
+                  var niceHotel = prompt ("What kind of hotel would you like? a. Three star?" + "b. Four star?" + "c. Five star?");
+                    if(niceHotel === "c") {
+                      console.log("This is Paris, you barely made it 3 days before you had to go home broke..");
+                    } else if(niceHotel === "b") {
+                      console.log("You had a great week in Paris, but you were also kidnapped and Liam Neeson didn't save you.");
+                    } else if(niceHotel === "a") {
+                      console.log("A 3 star 'hotel' in Paris means hostel, and sadly, all of your luggage was stolen. You were also kidnapped and Liam Neeson didn't save you.");
+                    }
+                } else if(niceCont === "b") {
+                    console.log("You will be sent to Brazil!");
+                    var niceHotel = prompt ("What kind of area would you like to stay? a. Party spots?" + "b. Rain forest?" + "c. Small coastal towns?");
+                      if(niceHotel === "c") {
+                        console.log("Welcome to Natal, a great town in Northeast Brazil");
+                          var natal = prompt("Does Natal seem ok (y/n)?");
+                            if (natal === "y"){
+                              console.log("You had a great stay and made it home happy");
+                            } else {
+                              console.log("That's crazy, Natal is the best, and you're just wrong.");
+                            }
+                      } else if(niceHotel === "b") {
+                        console.log("You head to Manaus, a small town in the Amazon");
+                          var water = prompt("Do you drink the water (y/n)?");
+                            if(water=== "y") {
+                              console.log("Yikes, your stomach was not prepared for that.. trip cut short!");
+                            } else {
+                              console.log("You have a great time in Manaus");
+                            }
+                      } else {
+                        console.log("You head to Rio! Copacabana here you come");
+                          var water = prompt("Do you drink the water (y/n)?");
+                            if(water=== "y") {
+                              console.log("Yikes, your stomach was not prepared for that.. trip cut short!");
+                            } {
+                              console.log("You drank too much, passed out on the beach, and woke up without your passport.. good luck!");
+                            }
+                          }
+                        }
+                    } else {
+                      var scared = prompt ("Are you afraid to go abroad (y/n)?");
+                        if(scared === "y") {
+                          console.log("Well that's too bad, you're missing out. You've been booked a ticket to Napa");
+                        } else {
+                          console.log("Hmm, your choices seem weird, try starting over");
+                        }
+                    }
+                  } else {
+                    console.log("With a budget that high you can go anywhere");
+                    var richTrip = prompt("Are you ok to go abroad (y/n)?");
+                      if (richTrip === "y") {
+                        console.log("You chose to go abroad. The options are Asia, or Australia")
+                          var richCont = prompt("Choose your location (select letter)? a. Asia" + " b. Australia");
+                            if(richCont === "a") {
+                                console.log("You will be sent to Japan first for the beginning of a 2 week excursion");
+                                var richHotel = prompt ("What kind of hotel would you like? a. Three star?" + "b. Four star?" + "c. Five star?");
+                                  if(richHotel === "c") {
+                                    console.log("This is Japan, you barely made it 3 days before you had to go home broke..");
+                                  } else if(richHotel === "b") {
+                                    console.log("You had a great week in Japan!");
+                                  } else if(richHotel === "a") {
+                                    console.log("You saved a decent amount of money with this option!");
+                                    var food = prompt("Do you want to splurge on meals now?");
+                                      if( food === "y") {
+                                        console.log("This was your best trip. Amazing food, location, and the hotel was great.")
+                                      } else {
+                                        console.log("Too bad, you ended up with food poisoning, but powered through to the end anyways.")
+                                      }
+                                  }
+                              } else if(richCont === "b") {
+                                  console.log("You will be sent to Australia!");
+                                  var richHotel = prompt ("What kind of area would you like to stay? a. Party spots?" + " b. desert?" + " c. Sydney?");
+                                    if(richHotel === "c") {
+                                      console.log("Welcome to Sydney!");
+                                        var natal = prompt("Does Sydney seem ok (y/n)?");
+                                          if (natal === "y"){
+                                            var tallAussies = prompt("You weren't intimidated by all the tall Aussies (y/n)?");
+                                              if(tallAussies === "y") {
+                                                console.log("Aren't we all? good thing you still had a great time.");
+                                              } else {
+                                                console.log("Impressive! Have fun.");
+                                              }
+                                          } else {
+                                            console.log("That's crazy, Sydney is amazing, and you're just wrong.");
+                                          }
+                                    } else if(richHotel === "b") {
+                                      console.log("You head to the desert, where a Kangaroo beats you up and leaves you..");
+                                    } else {
+                                      console.log("You head to Melbourne!");
+                                        var water = prompt("How's the Fosters? (good/bad)?");
+                                          if(water=== "good") {
+                                            console.log("Glad you enjoyed it! Hope the price wasn't too bad");
+                                          } else {
+                                            console.log("That's unfortunate to hear, you've clearly not done your research ahead of time.");
+                                          }
+                                        }
+                                      }
+                                  } else {
+                                    var scared = prompt ("Are you afraid to go abroad (y/n)?");
+                                      if(scared === "y") {
+                                        console.log("Well that's too bad, you're missing out. You've been booked a ticket to Napa");
+                                      } else {
+                                        console.log("Hmm, your choices seem weird, try starting over");
+                                      }
+                                  }
+                                }
