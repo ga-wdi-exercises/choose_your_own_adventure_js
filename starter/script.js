e0696ba7598765e7d7e766fe7ba40d4a4b3f3e6a
Roshana Devkota <roshana.devkota@gmail.COM>
2015-10-15 08:52:22 -0400

    Incomplete working file

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,21 @@
 // put code here!
+var userInput = prompt ("Welcome to adventure park. Please type l for kids park and r for adult adventure land");
+
+if (userInput == "l") {
+  console.log("At the adventure park there are rides for everyone. If speed and adventure aren't for you, we have carousel, wave swinger. Don't miss them");
+  var responseByKids = prompt("Please decide which ride you want to go. Press 1 for Blue Ridge Tollway, 2. for flying eagles, 3 for Dodgem");
+  if (responseByKids == 1)
+  {
+    var blueRidge = prompt ("Opps there is long line to wait you want to wait for next line or go to this one press w to wait or n to go new line");
+    if (blueRidge == 'n')
+    {
+      console.log("wait here please");
+    }
+    else if (Blue)
+    {
+      console.log("wait here please");
+    }
+  }
+}
+else if (userInput == "r"){
+}
