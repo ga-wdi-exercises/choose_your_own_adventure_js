744f9298dee0ca04ec51048b8a70700302f0d753
Elise Kain <elise.kain@gmail.com>
2015-10-14 20:15:52 -0400

    Psuedo Code in comments and script tag moved to head.

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,31 @@
-// put code here!
+// The Mystery of the Lost Locket
+//
+// - What is your name?
+//     - Erin
+// - You’re just who we’ve been looking for! You come highly recommended as an expert sleuth. We need your help solving the mystery of the Lost Locket! Will you accept the assignment? (Y/N)
+//     - Y
+//         - Thank you! We can enlist the help of some other detectives in this mystery. Who would you rather have help you: Nancy Drew or The Hardy Boys? (N/H)
+//             - N
+//                 - Here’s your brief on the assignment. Nancy’s eating lunch at the The Flying Fish at the harbor. We need you to go pick her up. You can choose to take the car, tandem bicycle or double kayak. (C/B/K)
+//                     - C
+//                         - The Rolls Royce is a pretty sweet ride. You two go around town picking up clues and drinking coffee and solve the mystery just in time for dinner. Turns out the locket was in the cup holder all this time! {END 4}
+//                     - B
+//                         - It turns out Nancy is not a good bicycle rider. On the way to the haunted mansion, she falls over, the bike frame is broken and you’re stuck in the middle of a country road and have to walk home. {END 3}
+//                     - K
+//                         - Oh no! You’re not a very good kayaker. How did you tip that over? All the clues are lost and the mystery is never solved. {END 2}
+//             - H
+//                 - The Hardy Boys are on their way! Where would you like them to meet you to start looking for clues: the Library or the Park? (L/P)
+//                     - L
+//                         - Mrs. Nelson is upset that you and the Hardy Boys are whispering so loudly in the Library. She taps her foot and crosses her arms and it’s just a matter of time before she kicks you all out. Do you ask her for help with the next clue? (Y/N)
+//                             - Y
+//                                 - With her help, you find the next clue. And the next one. And the next one. Except it turns out they aren’t clues at all, but false leads. By the time you realize she was misleading you, Mrs. Nelson is on a plane to South America with the locket around her neck. {END 5}
+//                             - N
+//                                 - You and the Hardy Boys leave the Library and get into the Rolls Royce. What do you find? The locket was in the cup holder this whole time! {END 6}
+//                     - P
+//                         - You all pile into the car to go to the Park and, when you stop at the drive-thru on the way, your soda spills all over the interior because it won’t fit into the cupholder. What’s in the bottom? The locket! Another mystery solved. {END 7}
+//     - N
+//         - No? What if we pay you a handsome sum? How much money would you need to take on this assignment? (Please enter a number.)
+//             - <=1,000
+//                 - I think we can make that work. Let me talk to the bosses to arrange payment. Never mind…we’re actually broke. {END 1}
+//             - while loop > 1,000
+//                 - You’re too rich for my blood. I don’t even think the locket’s worth that much. Can you charge a lower fee? How much? (Loop)
