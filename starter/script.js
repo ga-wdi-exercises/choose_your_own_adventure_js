15a90df37479d2f54ce939b850474f452e1818f9
Thomas Clark <tclark@soteriagiving.com>
2015-10-14 20:18:43 -0400

    sudocode

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,35 @@
-// put code here!
-console.log("Hello World")
-pizza
+Game:
+
+You journey to a remote island looking for treasure
+
+Mode of Transportation
+1. Boat
+2. Plane
+3. Helicopter
+4. Swim (it’s not that far)
+
+If you take a boat you get halfway there, a great white jumps out of the ocean eats you and your boats continues to move running into the beach and blowing up into millions of pieces.
+
+Plane: turbulent with a harsh landing rupturing one of your kidneys, you make it to the island but you’re in a lot of pain
+
+Helicopter: You get to the other side smoothly, except when landing you hit some pine trees and the left side of the helicopter slams into the ground crushing the pilot, who wasn’t buckled in, and breaking the three of the four blades.
+
+Swim: You jump and befriend a seal — seal becomes your true lover and your life is complete
+
+Once on the island:
+- plane ride: you bandage up your wound with a first aid kit you found in the plane; your pilot was supposed to guide you and split the profit, since he’s dead you get a little excited — it’s all yours now!
+- You get out of the helicopter unscathed and you set out on your journey for hidden treasure
+
+On your way you find an alligator pit with a loose bridge
+- you go around it; length of the pit was deceiving and you die of thirst
+- you run across it - you slip landing right in an alligator’s mouth
+- you cartwheel across it - the alligators are amused; they become submissive and watch
+- you cross it carefully; make it to the other side but it takes over an hour - your nerves are shot and you need a rest
+
+Take the rest or move on or slow down?
+- you take the rest and you have a heart attach
+- you keep going and you find a man who will guide you to the treasure
+- you walk slowly and cautiously
+
+Go with the guide  - he uses you to do the heavy lifting then kills you after you find the treasure - you will be remembered
+You walk slowly and cautiously and find the treasure
