7174987c1506bcc1d5b569d8391063ea36ae6022
Thomas Clark <tclark@soteriagiving.com>
2015-10-15 08:14:25 -0400

    add if statements

--- starter/script.js
+++ starter/script.js
@@ -3,31 +3,43 @@ console.log("************")
 }
 console.log("You want to jorney to a remote island to seek treasure...")
 buffer()
-var pathInput = ("You can either take a Boat, a Plane, a Helicopter, or Swim..(please enter ('b' or 'p' or 'h' or 's')")
+var pathInput = prompt("You can either take a Boat, a Plane, a Helicopter, or Swim..(please enter ('b' or 'p' or 'h' or 's')")
 
 if (pathInput == "b")
 console.log("You take the boat, you get half way there, a great white breach the surface, eats you in one bit and returs to the deep blue...Your boat, while on autopilot, crashes and burns on the island")
+buffer()
+if (pathInput)
 
-Plane: turbulent with a harsh landing rupturing one of your kidneys, you make it to the island but you’re in a lot of pain
+if(pathInput == "p"){
+  console.log("You take a plane, but the ride is turbulent with a harsh landing rupturing one of your kidneys, you make it to the island but you’re in a lot of pain")
+  buffer()
 
-Helicopter: You get to the other side smoothly, except when landing you hit some pine trees and the left side of the helicopter slams into the ground crushing the pilot, who wasn’t buckled in, and breaking the three of the four blades.
+  if(pathInput == "h"){
+    console.log("Helicopter: You get to the other side smoothly, except when landing you hit some trees and the left side of the helicopter slams into the ground crushing the pilot, who wasn’t buckled in, and breaking the three of the four blades.")
 
-Swim: You jump and befriend a seal — seal becomes your true lover and your life is complete
+if(pathInput == "s"){
+  console.log("You jump and befriend a seal — seal becomes your true lover and your life is complete...\n - your hunger for treasure diminishes")
+}
 
-Once on the island:
-- plane ride: you bandage up your wound with a first aid kit you found in the plane; your pilot was supposed to guide you and split the profit, since he’s dead you get a little excited — it’s all yours now!
-- You get out of the helicopter unscathed and you set out on your journey for hidden treasure
 
-On your way you find an alligator pit with a loose bridge
-- you go around it; length of the pit was deceiving and you die of thirst
-- you run across it - you slip landing right in an alligator’s mouth
-- you cartwheel across it - the alligators are amused; they become submissive and watch
-- you cross it carefully; make it to the other side but it takes over an hour - your nerves are shot and you need a rest
+// Helicopter: You get to the other side smoothly, except when landing you hit some pine trees and the left side of the helicopter slams into the ground crushing the pilot, who wasn’t buckled in, and breaking the three of the four blades.
+//
+// Swim: You jump and befriend a seal — seal becomes your true lover and your life is complete
 
-Take the rest or move on or slow down?
-- you take the rest and you have a heart attach
-- you keep going and you find a man who will guide you to the treasure
-- you walk slowly and cautiously
-
-Go with the guide  - he uses you to do the heavy lifting then kills you after you find the treasure - you will be remembered
-You walk slowly and cautiously and find the treasure
+// Once on the island:
+// - plane ride: you bandage up your wound with a first aid kit you found in the plane; your pilot was supposed to guide you and split the profit, since he’s dead you get a little excited — it’s all yours now!
+// - You get out of the helicopter unscathed and you set out on your journey for hidden treasure
+//
+// On your way you find an alligator pit with a loose bridge
+// - you go around it; length of the pit was deceiving and you die of thirst
+// - you run across it - you slip landing right in an alligator’s mouth
+// - you cartwheel across it - the alligators are amused; they become submissive and watch
+// - you cross it carefully; make it to the other side but it takes over an hour - your nerves are shot and you need a rest
+//
+// Take the rest or move on or slow down?
+// - you take the rest and you have a heart attach
+// - you keep going and you find a man who will guide you to the treasure
+// - you walk slowly and cautiously
+//
+// Go with the guide  - he uses you to do the heavy lifting then kills you after you find the treasure - you will be remembered
+// You walk slowly and cautiously and find the treasure
