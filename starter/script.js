5b5ed59ae846fa111395d34265fb660df039d834
jen pengelly <jgpengelly@gmail.com>
2015-10-15 18:56:02 -0400

    index: enter page title. script: add first user prompt.

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,13 @@
-// put code here!
+console.log("Something about the sorting hat")
+var userName = prompt("What is your name?")
+
+// function getName(userName)
+//   console.log("What is your name?");
+//   return prompt("Please enter your name?")
+
+// getName()
+console.log("Welcome, " + userName + ". Let's get you Sorted.")
+
+// function getPlayerMove(move) {
+//     return move || getInput();
+// }
