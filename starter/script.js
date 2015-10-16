7834bbfaa16b8dba52db5b4f57e6130816991cef
Jared Nichols <jnichols300@gmail.com>
2015-10-16 00:13:20 -0400

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,32 @@
 // put code here!
+console.log("Hi, welcome to the best adventure.");
+var question = prompt("Would you rather fly to North Korea or Mars?");
+  if (question == 'North Korea'){
+    console.log("Welcome to glorious country.")
+    var northKorea = prompt( "Type 'yes' if you want to stay here or type 'y' to stay here or 'n' to maybe go somewhere else." )
+      if (northKorea == 'yes'){
+        console.log("You are winner of cheeseburgers.")
+            }
+            };
+      if (northKorea == 'y'){
+        console.log("You are winner of 햄")
+            }
+      if (northKorea == 'n'){
+        console.log("lol you can't leave sorry")
+      }
+  if (question == 'Mars'){
+        console.log("Welcome to space where no one can hear you scream.")
+        var marsWelcome = prompt( "Would you like to meander through the space flowers? Type 'Sure'. Or would you rather find alien life? Type 'nascar'. ORRRRRR you could count your toes. Type '10' for this option." )
+          if (marsWelcome == 'Sure'){
+            console.log("Space flowers aren't real better luck next time.")
+                }
+                };
+          if (marsWelcome == 'nascar'){
+            console.log("You found alien life! Congratulations! It looks like pudding, smells like pineapple and behaves like a dog.")
+                }
+          if (marsWelcome == 10){
+            console.log("At least you can count! Good for you!")
+          }
+    else {
+      console.log("You didn't choose the right thing!")
+    }
