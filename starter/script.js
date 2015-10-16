adcecc4c3b54c50e20b83648190650d144618632
Thomas Clark <tclark@soteriagiving.com>
2015-10-16 04:54:36 -0400

    add most of code

--- starter/script.js
+++ starter/script.js
@@ -1,45 +1,35 @@
-Var buffer = function(){
-console.log("************")
+var buffer = function(){
+  console.log("*******************************")
 }
-console.log("You want to jorney to a remote island to seek treasure...")
-buffer()
-var pathInput = prompt("You can either take a Boat, a Plane, a Helicopter, or Swim..(please enter ('b' or 'p' or 'h' or 's')")
 
-if (pathInput == "b")
-console.log("You take the boat, you get half way there, a great white breach the surface, eats you in one bit and returs to the deep blue...Your boat, while on autopilot, crashes and burns on the island")
+console.log("You want to travel to a deserted island...")
 buffer()
-if (pathInput)
+var pathInput = prompt("You can either take a plane or a helicopter..(Please enter 'p' or 'h')")
 
 if(pathInput == "p"){
-  console.log("You take a plane, but the ride is turbulent with a harsh landing rupturing one of your kidneys, you make it to the island but you’re in a lot of pain")
+  console.log("You take the plane and everything is going well, in fact - you're ahead of scuedule. You hit some very bad turbulance and the plane crashes; you hit your head during the crash and it's all bloody now.")
   buffer()
-
-  if(pathInput == "h"){
-    console.log("Helicopter: You get to the other side smoothly, except when landing you hit some trees and the left side of the helicopter slams into the ground crushing the pilot, who wasn’t buckled in, and breaking the three of the four blades.")
-
-if(pathInput == "s"){
-  console.log("You jump and befriend a seal — seal becomes your true lover and your life is complete...\n - your hunger for treasure diminishes")
-}
-
-
-// Helicopter: You get to the other side smoothly, except when landing you hit some pine trees and the left side of the helicopter slams into the ground crushing the pilot, who wasn’t buckled in, and breaking the three of the four blades.
-//
-// Swim: You jump and befriend a seal — seal becomes your true lover and your life is complete
-
-// Once on the island:
-// - plane ride: you bandage up your wound with a first aid kit you found in the plane; your pilot was supposed to guide you and split the profit, since he’s dead you get a little excited — it’s all yours now!
-// - You get out of the helicopter unscathed and you set out on your journey for hidden treasure
-//
-// On your way you find an alligator pit with a loose bridge
-// - you go around it; length of the pit was deceiving and you die of thirst
-// - you run across it - you slip landing right in an alligator’s mouth
-// - you cartwheel across it - the alligators are amused; they become submissive and watch
-// - you cross it carefully; make it to the other side but it takes over an hour - your nerves are shot and you need a rest
-//
-// Take the rest or move on or slow down?
-// - you take the rest and you have a heart attach
-// - you keep going and you find a man who will guide you to the treasure
-// - you walk slowly and cautiously
-//
-// Go with the guide  - he uses you to do the heavy lifting then kills you after you find the treasure - you will be remembered
-// You walk slowly and cautiously and find the treasure
+  var responsePlaneCrash = prompt("You wounder what you want to do ... and decide to\n('b') - bandage yourself up with the left over medical supplies you could find in the cock pit.\n('w') - take a rest and wait until you feel better\n('d') - do nothing.")
+  if(responsePlaneCrash== 'b'){
+    console.log("You crawl to the cockpit, find the medical supplies, bandage up your bloddy head, and keep going - you want the damn treasure...blood dries; leaving the plane you see a man who has come for the same thing but he's obviously been on the island for many years and looks .")
+    var rabidManResponse= prompt("He asks a question, 'How many more miles to the treasure you're looking for?'...\n('any number') - Confidently you\n('a') - you tell him it's 40 miles from where you stand")
+    if(rabidManResponse== 'a'){
+      console.log("The rabid man charges at you and eats you alive.... he's hungry and there's nothing to eat on the island, he can't help it.")
+      buffer()
+    }
+    else {
+      while(rabidManResponse < 100){
+        var rabidManResponse = prompt("That's no where near how far it is")
+      }
+      console.log("The rabid dude shreeks, 'Wow " + rabidManResponse + " you must have been studying this place for years!' You tell him to wait there while you go to the bathroom in the woods (he's stupid). You continue on your journey and finally get to....Burger King, isn't that place golden?'" )
+      buffer()
+    }
+  }
+  else if(responseToRabidMan == 'w'){
+    console.log("You thing the best thing to do is rest and wait to start feeling better?'")
+    buffer()
+    var WaitingAround = prompt("Still a little out of it from the crash, you bandage up your head as well as you can but you start feeling woosy....\n(s) - .. All the blood rushes away from your head, starving you of oxygen and you die quickly in a slow manner\n('f') - you fight through the woozy feeling start crawling and slowly work your way to you feet safely.")
+    if(WaitingAround == 's'){
+      console.log("You're dead!")
+      buffer()
+    
