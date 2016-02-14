1484e8a18582379babca07b8c15a26de611360f3
Anissa Jones <anissajones10@gmail.com>
2016-02-14 13:10:21 -0500

    Decided on adventure and the mini goals.

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,14 @@
-// put code here!
+//Mythical Beauty War
+
+//Goal is to get all the makeup, hair, and clothing neccessities for the Princess. And bring
+// them back so that she can go to THE red carpet event.
+
+//Rainbow throwup = lip sticks
+//Phoenix ashes = smokey eyes
+//Mama Troll Buttermilk icecream = foundation
+//Fairy dust = shimmer and highlights
+//Cracken ink = eye liner and mascara
+//Dragon scales = dress
+//Mermaid hair = extentions or weave
+//Medusa hair = necklace
+//Elf workers gift = heels/shoes
