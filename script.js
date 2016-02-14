f9cfb8f772e277976ed08ff890471ea02801d46b
caseyagil <caseyagil@gmail.com>
2016-02-14 14:52:39 -0500

    Starting

---
+++ script.js
@@ -0,0 +1,11 @@
+confirm("Are you ready to begin your Quest?");
+var age = 18;
+prompt("What is your age Padawan"?);
+console("You are not allowed to begin until you are 18");
+if(age < 18)
+{
+console("you are not yet skilled, Padawan");
+}
+else {
+prompt("What is your real name");
+}
