0d525925f22ee025991eb22f82d75ea568159528
Anissa Jones <anissajones10@gmail.com>
2016-02-15 14:32:37 -0500

    Cut back on options and finished outline with choices.

--- starter/script.js
+++ starter/script.js
@@ -3,12 +3,109 @@
 //Goal is to get all the makeup, hair, and clothing neccessities for the Princess. And bring
 // them back so that she can go to THE red carpet event.
 
-//Rainbow throwup = lip sticks
-//Phoenix ashes = smokey eyes
-//Mama Troll Buttermilk icecream = foundation
-//Fairy dust = shimmer and highlights
-//Cracken ink = eye liner and mascara
-//Dragon scales = dress
-//Mermaid hair = extentions or weave
-//Medusa hair = necklace
 //Elf workers gift = heels/shoes
+//Unicorn Rainbow throwup = lip sticks
+//Medusa hair = necklace
+//Dragon scales = dress
+//Fairy dust = shimmer and highlights
+//Mermaid hair = extentions or weave
+//Phoenix ashes = smokey eyes
+
+
+
+//Print to screen: The Millinnial Princess Pink Apply Everly Reignbeau aka Princess Pear of Mac'Ephora Lusha Brown Land is getting ready for
+//her first Golden Carpet event.
+//She has summoned you to her chambers. What is your name great warrior?
+//User enters name.
+//Princess Oh 'Users Name', all the things I have are so common, the other Princesses will
+//surely have the same thing on. I need you to find me the rarest of the rare things that
+//will make my beauty stand out above all the others. Will you help.
+//(y)es, gladly, were do I start?,
+      //First go to the elves in the basement, I hear they make shoes.
+//(N)o, unless you fill my pockets with gold,
+      //Fine you heathen, I'll pay. Now go to the elves in the basement, I hear they make shoes.
+// or (B)ut your beauty outshines the others, you could wear a trashbag.
+      //Gross, I would rather die. Please just go to the elves in the basement, I hear they make shoes.
+
+                    //All at elves
+//The elves are working hard
+//You ask for help. One elf steps forward and says
+//They are tired and cannot take on anymore orders. The princesses from around the world
+//Trapped them here. However, if he frees them, they will make one last pair for Princess Pear
+//Choose : (y)es of course,
+      //Thank you, thank you. *Gain shoes* Elves tell user about Yum Yum Forest and Unicorns
+            //Spots Unicorn and unicorn gets scared and throws up Rainbows.
+            //Offers to give (R)ainbow lipstick in exchange for users lunch.
+            //Choose: (y)es and trade *Gain Unicorn Rainbow Lipstick*
+                    //Unicorn leaves and you wonder forest and stubble upon fairies.
+            //or (n)o, try to kill unicorn to sell it and steal lipstick
+                    //Unicorn kicks user, knocks user out, and burries lipstick so it can't be used.
+                    //Fairies find user unconcious
+//(n)o, Ill kill all but one and that one will make the shoes!
+      //All elves attack user and push him through portal to Medusa's layer
+            //User is in candle lit bedroom. He tries to leave through a door but
+            //its a closet full of men turned to stone. User realizes where they are and panics
+            //User knows (s)he is in Medusa's layer and thinks quickly.
+            //he blows out all the candles so that it is pitch black.
+            //Medusa walks in and is confused why it is dark, as she is lighting a candle.
+            //Choose: Blow candle out and (s)neak away before she catches you
+                      //User finds way out of layer and is back in Yum Yum Forest
+                      //where you stumble upon Fairies
+            //OR: Blow candle out, use your (c)unning words to seduce her, have one night stand,
+            //steal Medusa Hair for a necklace and sneak out before she wakes. *Gain Medusa necklace*
+                      //User finds way out of layer and is back in Yum Yum Forest
+                      //Where you stumble upon fairies.
+
+                      //All at Fairies
+//Fairies are sweet and loving, healing users wounds and offering tea and cookies.
+//User eats and parties with them all night and the Fairy king makes user honorary guest.
+//You notice all their meals appear with fairy dust which would be nice for Princess Pear
+//Choose: (A)sk for a bit for the princess *Gains Fairy Face Shimmer*
+      //The Fairy King is pleased and tells you the secret of Dragon scales. You travel
+      //Firey cove.
+//OR: Get the frairies (d)runk and steal it. *Gains Fairy Face Shimmer*
+      //The king discovers your deed as you are leaving and curses you to Firey Cove.
+
+                    //All at Firey Cove with Dragons.
+//You are scared as you walk amongst dozens of sleeping Dragons.
+//Choose: Change your mind and exit as quickly and stealthily as possible.
+      //At the end of the Cove is a watery OASIS
+//OR: Use a bit of your fiary dust to blind them all. Skinning one of the smaller Dragons
+//and exiting as possible. *Gaind Dragon Scale dress*
+      //At the end of the Cove is a watery OASIS
+
+                    //All At Mermaid Watery Oasis
+//There are mermaids everywhere, singing and combing their hair.
+//They see you and all fawn and dote on you. They are not allowed to be around humans
+//but have been lacking excitement.
+//You tell them of your journey and all its excitement.
+//They are so enamored with you and excited by your travels, they offer to let you pick
+//a treasure you have yet to gain.
+    //Choose: Gain *(D)ragon Scale Dress.*
+    //OR: Gain *(M)edusa necklace*
+    //OR: Gain *(U)nicorn Rainbow Lipstick*
+//User chooses a gift. As you thank the mermaid and prepare to leave you are drawn back to their
+//beauty. They don't want you to leave.
+      //Choose: You lie and say you will come back after your mission.
+            //Mermaids believe you and give you some of their hair to remember them by.
+            //Gain *Mermaid Weave*
+            //At the end of the beach is a dying Phoenix.
+      //OR: You stay, have weird mermaid orgy and leave while they are still cuddling and entangled in each other
+            //You make it to the end of the beach were you see a dying Phoenix
+
+                  //All at Phoenix
+//The Phoenix is kind and will grant you a wish before you die because he likes the company.
+//But first you must guess which door numbered 1 -20 will take you home.
+//20 doors appear. The Phoenix is happy you will be staying with him a while. He grins.
+//Choose: A number until you guess the right door.
+
+//Once user guesses correct door, Phoenix dies. *Gain Phoenix Smokey Eye Kit.*
+
+                //All At home.
+//You are now home. It is one hour before Princess Pear has to leave. She is in a panic,
+//When you arrive.
+//She asks you what you brought.
+//User empties scachel.
+//IF you have 6-7 items = Glory and the Princess hand in marriage.
+//IF you have 3-5 items = Land to grow a family and no more missions
+//IF you have 2 or less = Banished back to Medusa's layer. 
