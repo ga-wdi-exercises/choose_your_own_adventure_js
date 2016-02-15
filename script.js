96aa55474b8ac41c490df495d669edee7cd7a317
Christine Movius <christine.movius@gmail.com>
2016-02-15 12:42:05 -0500

    created first prompt and Hermia's first prompt in JS.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// put code here!
+var start_number = prompt("Good morrow, my fair adventurer. Please pick a number 1 through 4 (i.e. 1, 2, 3, or 4):")
+
+if (start_number === "1") {
+  //console.log('Hermia');
+  var hermia_intro = prompt("You are Hermia. You are in love with Lysander, but your father insists that you marry Demetrius. Demetrius is attractive, but a jack-ass. Lysander has a heart of gold. Your father threatens that if you do not marry Demetrius (M), you must either face death (D) or become a nun (N). Lysander pleads that you run away with him and elope (R). What is your choice (M, D, N, R)?")
+}
