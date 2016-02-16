a48cc77d2f0d0795c79b98f86cb067a41e62ea03
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-16 10:39:40 -0500

    added starter code

--- starter/script.js
+++ starter/script.js
@@ -1,2 +1,41 @@
 confirm("Are you over 18? Clicking OK proves this beyond a doubt. It's science.");
+var setting = "Setting for most generic Bodice Ripper Ever";
+confirm(setting);
+console.log(setting);
+
+var userInput = prompt("Are you a lad, a lass, or niether?");
+    if(userInput === "lad"){
+        confirm("lad does this and that");
+    console.log("Stuff happens to lad");
+var ladChoice = prompt("Lad does this and makes this choice: type 'keep her' or 'dont keep her'");
+    {if (ladChoice==="keep her"){
+        console.log("Lad keeps her and blah blah blah.")
+    }
+    else {
+        console.log("Lad doesn't keep her blay blay blay")
+    }}}
+
+    if(userInput === "lass"){
+        confirm("lass does such and such");
+    console.log("Stuff happens to lass");}
+var lassChoice = prompt("Lass chooses highwayman or farmboy'");
+    {if (lassChoice==="highwayman"){
+        console.log("lass deals with highwayman")
+    }
+    else {
+        console.log("Lass deals with farmboy")
+    }}
+
+if (userInput === "neither"){
+var NunOrNot = prompt("you are a nun, yes or no")
+   if (NunOrNot==="yes"){
+            console.log("NUN ENDING");
+        }
+    }
+    else if (NunOrNot ==="no"){
+   confirm("Then you are father gross guy. No use denying it.")
+   console.log("this is what happens to father gross guy...")
+}
+
+
 // put code here!
