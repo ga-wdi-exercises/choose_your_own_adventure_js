cf562f071060c926442e5d457a1b2cd15147d925
Melissa Miller <lis.d.miller@gmail.com>
2016-02-16 13:56:20 -0500

    Adds tomorrowland options

--- starter/script.js
+++ starter/script.js
@@ -20,7 +20,7 @@ function theHub()
   if(response == "R" || response == "r")
   {
     window.alert("You've chosen Tomorrowland, the land of the future that always was and never will be.");
-    tomorrowland;
+    tomorrowland();
   }
   else if(response == "S" || response == "s")
   {
@@ -32,38 +32,67 @@ function theHub()
   }
   else
   {
-    console.log("Else path");
+    window.alert("I know the choice is hard, take your time.");
+    theHub();
   }
 } // end theHub
 
 function tomorrowland()
 {
   window.alert("Welcome to Tomorrowland! The sun shines off the silver buildings and there is alien writing on many of the buildings.");
+  tomorrowlandRides();
+} // end tomorrowland
+
+function tomorrowlandRides()
+{
   var response = window.prompt("What kind of ride do you want to go on?"
   + "\n A thrill ride (T)"
   + "\n Something funny (F)"
   + "\n A nostalgic classic (N)"
   + "\n Something that will cause pain and regret (P)");
 
-  if(response == "T")
+  if(response == "T" || response == "t")
   {
-    console.log("Space mountain");
+    window.alert("Thrills huh? Let's go on Space Mountain! It's an indoor roller coaster in the dark!");
+    window.alert("WHEEEEEEEEEEEEEeeeeeeee!!!");
+    endOfTomorrowlandRide();
   }
-  else if(response == "F")
+  else if(response == "F" || response == "f")
   {
-    console.log("Monsters Inc Laugh Floor");
+    window.alert("Laughs? You're going to love Monsters Inc Laugh Floor! It's an interactive comedy show!");
+    window.alert("Hellooooooo humans!")
+    endOfTomorrowlandRide();
   }
-  else if(response == "N")
+  else if(response == "N" || response == "n")
   {
-    console.log("Carousel of Progress");
+    window.alert("A classic. Good choice! Let's head over to the Carousel of Progress, designed by Walt Disney's original team!");
+    window.alert("There's a great big beautiful tomorrow, Shining at the end of every day...");
+    endOfTomorrowlandRide();
   }
-  else if(response == "P")
+  else if(response == "P" || response == "p")
   {
-    console.log("Stitch's Great Escape");
+    window.alert("Really? Paain? You're going to regret this...But let's head over to Stitch's Great Escape");
+    window.alert("Chili dog burps...forever");
+    endOfTomorrowlandRide();
   }
   else {
     {
-      console.log("try again");
+      window.alert("It seems like you don't like these choices. Maybe you'd prefer to return to the Hub?")
+      theHub();
     }
   }
-} // end tomorrowland
+} // end tomorrowlandRides
+
+function endOfTomorrowlandRide()
+{
+  var response = window.prompt("That was fun! Do you want to go on another ride? (Y for yes)")
+  if(response == "Y" || response == "y")
+  {
+    tomorrowlandRides();
+  }
+  else
+  {
+    window.alert("Ok! Let's move on to Fantasyland!");
+    fantasyland();
+  }
+} // end endOfTomorrowlandRide
