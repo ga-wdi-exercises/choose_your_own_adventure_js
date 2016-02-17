7a1a0dbe060ce1e85717f0078be9de4e7432f98c
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-17 01:00:25 -0500

    Re-broke the code I fixed. Now I can't get it to work again.

--- starter/script.js
+++ starter/script.js
@@ -3,23 +3,29 @@ alert("It is a steamy, wind-swept night and you are alone in your grammaw's hous
 alert("The internet is out and all she has for entertainment are these crusty cheese-ball romance novels. You have a choice: Go in the living room and help her file her bunions or pretend to read one of these things and let her think you're really into it.");
 /*Read or File Bunions?*/
 var BunionsOrRead = prompt("Now, what will it be honey-child, Mammaw's Bunions or Skanky Bodice Ripper? (Type 'B' for 'Bunions' or 'R' for 'Read the damn book').");
-    if(BunionsOrRead === "B" || BunionsOrRead === "b" || BunionsOrRead === "Bunions"){
-      alert("You chose your fate, bro. Go get the cheese-grater and the lotion! THE END.");
-    }
-    else if (BunionsOrRead === "R" || BunionsOrRead === "r" || BunionsOrRead === "Read"){
-    alert("You settle down and pick a book.");
-    //&&&
-    var BookPick1 = prompt("They have terrible titles. Who wrote this dribble?! Oh, well. This one looks good. It's called 'Pirate Saga: A Sea of Flaming Booty'. Do you want to open it? Yes or No?");
-      if(BookPick1 === "no"){
-        var BookPick2 = prompt("Yeah, me neither. Oooh! How about 'There's Something In The Milk Maid, part 4?' That's gotta be good, right? ");
-          if (BookPick2 === "no"){
-            prompt("Fine, fine...what was I  thinking? Oh! This one's perfect! It's called 'Brogrammers in love: Escape from Java's Crypt.");
-          }
+    if(BunionsOrRead === "B" || BunionsOrRead === "b" || BunionsOrRead === "Bunions"){ alert("You chose your fate, bro. Go get the cheese-grater and the lotion! THE END.");}
+
+    else if (BunionsOrRead === "R" || BunionsOrRead === "r" || BunionsOrRead === "Read")
+    { alert("You settle down and pick a book.");}
+    
+      var BookPick1 = prompt("This one looks good. It's called 'Pirate Saga: A Sea of Flaming Booty'. Do you want to open it? Yes or No?");
+
+      if(BookPick1 === "yes"){
+        alert("Pirate's tale!");/*and*/
+        var Sharks = prompt("make a pirate choice!");/*and if the choice is yes */
+        if(Sharks === "yes"){alert("You're doing it!")}
+    /*but if sharks is no*/else if(Sharks === "no"){alert("SHE GETS EATEN!")}
+    /*if BookPick1 is no, then...*/
+      if(BookPick1 === "no"){var BookPick2 = prompt("Yeah, me neither. Oooh! How about 'There's Something In The Milk Maid, part 4?' That's gotta be good, right? ");
+        if(BookPick2 === "yes"){alert("Milk Maid Tale!")}/*and then*/
+        var Virtue = prompt("Make a maidens choice! DO or DON'T?");/*and if the choice is yes*/
+        if (Virtue === "DO"){alert("Maiden Ending 1");
+        if (Virtue === "DON'T"){alert("Maiden Ending 2!");}
+
+        if (BookPick2 === "no"){prompt("Fine, fine...what was I  thinking? Oh! This one's perfect! It's called 'Brogrammers in love: Escape from Java's Crypt.");}}
+        }
 
 
-
-
-    }
     else if (BunionsOrRead !== "B" || BunionsOrRead !== "R"){
       alert("Oh? Not playing at all today? Okay... it's the BUNIONS for you!");
 
