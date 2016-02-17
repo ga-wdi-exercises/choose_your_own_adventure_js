7c08f67d1781fec4c567643e9cf45a5764b699af
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-16 22:56:06 -0500

    improved upon JS file, still in progress

--- starter/script.js
+++ starter/script.js
@@ -1,12 +1,23 @@
 confirm("Are you over 18? Clicking OK proves this beyond a doubt. It's science.");
-var setting = "Setting for most generic Bodice Ripper Ever";
+/* Set the scene and print it out*/
+var setting = "It is a steamy, wind-swept night and you are alone in your grammaw's house. It is boring.";
 confirm(setting);
-console.log(setting);
+alert("The internet is out and all she has for entertainment are these crusty cheese-ball romance novels. You have a choice: Go in the living room and help her file her bunions or pretend to read one of these things and let her think you're really into it.")
+/*Read or File Bunions?*/
+var BunionsOrRead = prompt("Read the console! Now, what will it be honey-child, Mammaw's Bunions or Skanky Bodice Ripper? (Type 'B' for 'Bunions' or 'R' for 'Read the damn book').")
+    if(BunionsOrRead === "B" || BunionsOrRead === "b" || BunionsOrRead === "Bunions"){
+      alert("You chose your fate, bro. Go get the cheese-grater and the lotion! THE END.")
+      };
+    else if(BunionsOrRead === "R" || BunionsOrRead === "r" || BunionsOrRead === "Read"){
+    confirm("You settle down and pick a book.");
+      };
 
-var userInput = prompt("Are you a lad, a lass, or niether?");
+
+var userInput = prompt("Are you a strapping young lad, a buxom, or niether?");
     if(userInput === "lad"){
         confirm("lad does this and that");
     console.log("Stuff happens to lad");
+/**/
 var ladChoice = prompt("Lad does this and makes this choice: type 'keep her' or 'dont keep her'");
     {if (ladChoice==="keep her"){
         console.log("Lad keeps her and blah blah blah.")
