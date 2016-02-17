dce0cc10597d246f2da11dd979ce48371cce6d06
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-17 00:00:15 -0500

    editing JS

--- starter/script.js
+++ starter/script.js
@@ -1,52 +1,28 @@
 confirm("Are you over 18? Clicking OK proves this beyond a doubt. It's science.");
 /* Set the scene and print it out*/
-var setting = "It is a steamy, wind-swept night and you are alone in your grammaw's house. It is boring.";
-confirm(setting);
+/*Later, move variables to the top*/
+alert("It is a steamy, wind-swept night and you are alone in your grammaw's house. It is boring.");
 alert("The internet is out and all she has for entertainment are these crusty cheese-ball romance novels. You have a choice: Go in the living room and help her file her bunions or pretend to read one of these things and let her think you're really into it.")
 /*Read or File Bunions?*/
-var BunionsOrRead = prompt("Read the console! Now, what will it be honey-child, Mammaw's Bunions or Skanky Bodice Ripper? (Type 'B' for 'Bunions' or 'R' for 'Read the damn book').")
+var BunionsOrRead = prompt("Now, what will it be honey-child, Mammaw's Bunions or Skanky Bodice Ripper? (Type 'B' for 'Bunions' or 'R' for 'Read the damn book').")
     if(BunionsOrRead === "B" || BunionsOrRead === "b" || BunionsOrRead === "Bunions"){
-      alert("You chose your fate, bro. Go get the cheese-grater and the lotion! THE END.")
+      alert("You chose your fate, bro. Go get the cheese-grater and the lotion! THE END.");
+    } //this doesn't end here. why?
+    else if (BunionsOrRead !== "B" || BunionsOrRead !== "R"){
+      alert("Oh? Not playing at all today? Okay... it's the BUNIONS for you!");
+    }
+    else if (BunionsOrRead === "R" || BunionsOrRead === "r" || BunionsOrRead === "Read"){
+    alert("You settle down and pick a book.");
+    /*Insert Loop here? Pick bad titles?*/
+    var BookPick1 =prompt("They have terrible titles. Who wrote this dribble?! Oh, well. This one looks good. It's called 'Pirate Saga: A Sea of Flaming Booty'. Do you want to open it? Yes or No?");
+      if(BookPick1 === "no"){
+        prompt("Yeah, me neither. Oooh! How about 'There's Something In The Milk Maid, vol 4?' That's gotta be good, right? ");
       };
-    else if(BunionsOrRead === "R" || BunionsOrRead === "r" || BunionsOrRead === "Read"){
-    confirm("You settle down and pick a book.");
-      };
-
-
-var userInput = prompt("Are you a strapping young lad, a buxom, or niether?");
-    if(userInput === "lad"){
-        confirm("lad does this and that");
-    console.log("Stuff happens to lad");
-/**/
-var ladChoice = prompt("Lad does this and makes this choice: type 'keep her' or 'dont keep her'");
-    {if (ladChoice==="keep her"){
-        console.log("Lad keeps her and blah blah blah.")
-    }
-    else {
-        console.log("Lad doesn't keep her blay blay blay")
-    }}}
-
-    if(userInput === "lass"){
-        confirm("lass does such and such");
-    console.log("Stuff happens to lass");}
-var lassChoice = prompt("Lass chooses highwayman or farmboy'");
-    {if (lassChoice==="highwayman"){
-        console.log("lass deals with highwayman")
-    }
-    else {
-        console.log("Lass deals with farmboy")
-    }}
-
-if (userInput === "neither"){
-var NunOrNot = prompt("you are a nun, yes or no")
-   if (NunOrNot==="yes"){
-            console.log("NUN ENDING");
-        }
-    }
-    else if (NunOrNot ==="no"){
-   confirm("Then you are father gross guy. No use denying it.")
-   console.log("this is what happens to father gross guy...")
+  }
 }
+
+
+
 
 
 // put code here!
