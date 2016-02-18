b9130d29366c5523df470186c46bb7078d1ee7de
Hilary Masland <hilarymasland@gmail.com>
2016-02-18 08:05:16 -0500

    Initial commit for Adventure Game

---
+++ starter/pseudo.js
@@ -0,0 +1,25 @@
+Bear on the Run
+
+"Hi! I hope you're all stretched out and ready to run!"
+var runner = prompt("What's your name?");
+  return {"Hello" + runner + "Let's start running!"
+}
+var terrain = prompt("Choose your terrain: (road, trail)"
+  switch(terrain) {
+    case "road":
+      text = "Cool!";
+    case "trail":
+      text = "Awesome!"
+  }
+
+
+
+
+  If Trail {
+    return "Nice choice! Would you like to run the Appalachian Trail or Rock Creek Park?"
+      If Appalachian {
+      How would you like to energize? (Chew Gels, Gaterade, Choclate)
+      }
+      If Rock Creek {
+      How would you like to energize? (Chew Gels, Gaterade, Chocolate)
+      }
