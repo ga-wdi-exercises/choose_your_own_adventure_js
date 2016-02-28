6d58d9c05b2dc6e0caf76aa3aa9bf11fa9a7e96d
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-28 17:09:29 -0500

    All initial errors fixed

--- starter/script.js
+++ starter/script.js
@@ -1,9 +1,9 @@
 // put code here!
 
 var name = prompt("what is your name?"); //.toUpperCase();
-  if (name = true); {
-  prompt("Welcome " + name +", Congratulations on your new job as a web developer intern. Your annual salary is $35,000 per year. I'm here to help you find an apartment.");
-};
+  if (name = true) {
+  prompt("Welcome " + name + ", Congratulations on your new job as a web developer intern. Your annual salary is $35,000 per year. I'm here to help you find an apartment.");
+}
 var choice = prompt("What part of the city would you like to live?NW, SW, NE, SE").toString().toUpperCase(); {// change input to string and uppercase
 
   if (choice == "NW") { // display info for NW and prompt user to type answer
@@ -47,7 +47,7 @@ var choice = prompt("What part of the city would you like to live?NW, SW, NE, SE
       Uber black pays more but uber x has fewer rules.");
 
         var UberXOrBlack = prompt("Do you decide on uber X or uber black? Type'X' for UberX or 'B' for Uber-Black").toString().toUpperCase(); { // decide if I want to keep using tostring or restrict user answer to only the 2 options
-          if (UberXOrBlack == "X"); {
+          if (UberXOrBlack == "X") {
             prompt("on uber x you must work 5 hours a day 7 days a week to come up with the rent. \
             You now have no social life outside of uber and your internship");
           }
@@ -59,33 +59,35 @@ var choice = prompt("What part of the city would you like to live?NW, SW, NE, SE
           }
 
           else {
-            return UberXOrBlack; // write a code to send user back to var Uber-Black
+            UberXOrBlack; // write a code to send user back to var Uber-Black
           }
         }
     }
-  else if (choice == "SW"); {
+  else if (choice == "SW") {
     prompt("Very Resonable choice. In fact it is so reasonable that 50,000 people just like you made the same choice \
     and Southwest becomes the the part of the city with the most rapidly increasing rents.  \
     You decide to sign a 5 year lease \to lock-in a rent you can afford.  You better love this internship and stay in DC for a while \
-     because with the $5,000 deposit you can't afford to move again any time soon")
-  };
+     because with the $5,000 deposit you can't afford to move again any time soon");
+  }
 
   else if (choice == "NE") {
     prompt("Northeast is a wise choice. Lots of up and coming nieghborhoods and your new place is in walking distance to your internship. \
     You no longer use your car on a regular basis.  Do you sell your car or keep it?");
 
       var sellOrKeep = prompt("Do you sell or keep your car? Type 'S' to sell it or 'K' to keep it");{
-        if (sellOrKeep == "S"); {
+        if (sellOrKeep == "S") {
           prompt("You sell your car and use the money to buy a new matching set of apple products; \
-          Macbook, Ipad, and Iphone now you can code in style!")
-        };
+          Macbook, Ipad, and Iphone now you can code in style!");
+        }
         else if (sellOrKeep == "K") {
           prompt("Hey you never know when you might need a car.  You use it for random roadtrips and as a backup when you need to go shopping. \
-          As a bonus it becomes a free billboard to advertise your new tech startup.")
-        };
+          As a bonus it becomes a free billboard to advertise your new tech startup.");
+        }
 
-        else;
-        return // write a code to return to the beginning of the NE section
+        else {
+          sellOrKeep;
+         // write a code to return to the beginning of the NE section
+        }
       }
   }
 
@@ -95,18 +97,20 @@ var choice = prompt("What part of the city would you like to live?NW, SW, NE, SE
 
       var apartmentLevel = prompt("Do you want the top floor or the basement? Type'T' for top floor or 'B' for Basement)").toUpperCase();
 
-        if (apartmentLevel = "T"); {
+        if (apartmentLevel = "T") {
         prompt("Choosing the top floor guarantees no one will be stomping above your head. \
         You can sleep in on the weekends with no interruptions. \
         The elevator breaks down after 2 weeks and you have to walk up 4 flights to your unit. \
-        On the bright side at least you don't have to pay for a gym membership with all those stairs.")
-        };
+        On the bright side at least you don't have to pay for a gym membership with all those stairs.");
+        }
 
         else if (apartmentLevel =="B") {
           prompt("The basement seems like a great choice, its cool in the summer time and easy access to bring things in and out your apartment. \
-          Your apartment is broken into and you learn the hard way easy access isn't always good.")
-        };
+          Your apartment is broken into and you learn the hard way easy access isn't always good.");
+        }
 
         else {
-          return; // write a code to take user back to the beginning of SE section
-        };
+          apartmentLevel; // write a code to take user back to the beginning of SE section
+        }
+      }
+}
