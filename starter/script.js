d6f48a8b10f82954cf697861205f87664ca134bd
Wes Reed <wesley.a.reed@gmail.com>
2016-03-19 19:49:11 -0400

    Initial commit. Add pilgram choice.

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,29 @@
-// put code here!
+var bumper = function() {
+  console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+};
+
+// var age = prompt("What's your age?");
+//
+// if (age < 13) {
+//   console.log("You're allowed to play, but at your own risk!");
+// }
+// else {
+//   console.log("Ok let's play!");
+// }
+// bumper();
+//
+// console.log("You find yourself at a Justin Bieber concert...and what's worse is you're actually singing along");
+// bumper();
+// console.log("Then the Biebs stops singing, and asks 'Does anyone want to race me?'");
+// bumper();
+//
+// var notafanAnswer = prompt("Do you want to race Justin? (Please enter 'y' or 'n')");
+
+console.log("You currently live in a place called 'City of Destruction'. No one living there seems to have any hope and everyone is convinced the city is about to be completely wiped out. So you decide set out on a pilgramage.");
+bumper();
+
+var pilgramChoice = prompt("You can head toward the 'Borough of Despond' or toward the 'Village of Morality' (Please enter 'b' or 'v').");
+
+if (pilgramChoice == 'b') {
+  console.log("You arrive the Borough of Despond and meet a man named'Dubious'");
+}
