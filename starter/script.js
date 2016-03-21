ca24e1eddfad06cbe3e17ebd19cfd6806acf6d9d
Habby Whyte <bahybaj@hotmail.com>
2016-03-21 01:58:27 -0400

    bare bones of morning to go

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,33 @@
 // put code here!
+var tantrumFree = function() {
+  console.log("**************************")
+}
+console.log ("You've overslept one morning and realized you have 25 minutes to get yourself and your kindergartener out the door")
+var wakeUpInput = prompt("You can either jolt out of bed or push the snooze button") (Please enter 'j' or 's')
+
+if (wakeUpInput) == "j"{
+  console.log("You jolt out of bed and begin dressing your kindergartener, when she decides she doesn't want to wear your option but rather her polka-dot pink Doc McStuffin shirt with her green striped duck pants."
+}
+var (reponseToToddler) = prompt("You realize that this very moment could make or break her blossoming kindergarten reputation" and decide to \n('a') - argue with her. We're talking reputation here people!...hers and yours!\n('g') - give in, toddlers have no reputation.  This may actually increase her self-confidence, while the other parents question your parenting skills\n('n')- negotiate with her.  She may know the latest trends that you may not be hip to and needs just a little guidance.")
+
+if(responseToToddler == 'a'){
+  console.log("You argue tooth and nail with her, when you realize you've wasted time, increased your blood pressure and will inevitably be late")
+  buffer()
+}
+
+else if (responseToToddler == 'g'){
+  console.log("With extra vigor, she happily puts on her clothes, saving you the extra minutes to brush your teeth.  Who cares what she wears anyways, you say to yourself")
+  buffer()
+  var toddlerReaction == prompt ("She asks you what's for breakfast?"\n('p') - Pancakes\n('o')'Not Pancakes');
+  if (responseToToddler =='p') {
+    console.log("She gives you her toothiest grin and runs down the stairs.  You follow her down the stairs, to pop in her pancakes in the oven, saving you a minute to finish getting dressed.");
+    tantrumFree();
+  }
+else if (responseToToddler =='o'){
+  console.log("Your toddler lets out a loud shriek and begins to whine.  She then refuses to take a bite, making you late.");
+  bumper();
+}
+
+else if (wakeUpInput) == "s"{
+  console.log("You groggily push the snooze button, surrending to the fact, that you'll be late regardless. Only to have your toddler jump onto your bed 3.5 seconds later, shaking all hope of gaining that extra 10 minutes of sleep")
+}
