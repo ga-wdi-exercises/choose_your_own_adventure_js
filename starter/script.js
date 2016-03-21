0cf43ba378127348843a2203b8ae7dd7c975670c
Matt <mvaden08@gmail.com>
2016-03-21 08:02:17 -0400

    Changes made to homework for ChooseYourOwnAdventure.

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,36 @@
 // put code here!
+/* GAME IDEA: OFF TO CLASS!!
+
+Output: Whats your name?
+Input: Player
+
+Output: Nice to meet you, PLAYER. What is your destination today?
+Input: General Assembly
+
+Output: Excellent! I
+
+
+I see you have 9am class and coming from SW. Since the bicycle is already your preferred method of travel, lets start planning your route. Will you be using bike lanes or sidewalks?
+Input 1: Bike lanes, dude! Too early in the morning to be dodging pedestrains on sidewalks, let alone cross walks.
+      Output: You follow the designated bike lanes all the way from SW, obeying most redlights and narrowly avoiding that driver from Virgnia in the silver Civic. You get to Pennsylvaina and find yourself stuck behind several other rider, loosing a few moments in the process. You do however make up for this lost time as you quickly dart through Lafayette Square, past Secret Service, and past the gridlock on K Street as you arrive at class, punctually and without a mark of dirt. You win!
+Input 2: Side walks - but only to avoid getting t-boned by cab drivers.
+      Output: You find yourself riding the sidewalks until coming to 15th street, whereby you narrowly miss crashing into a huge group of school children on a field trip and end up crashing into the Presidents motorcade as it emerges from seemingly nowhere and onto 15th Street. After getting questioned and cleaned up by Secret Service for a half hour, you narrowly avoid detainment. You arrive late to class, flustered and in distress. Too bad - try again tomorrow! */
+
+
+var bikeLanes ;
+var sidewalks;
+var greeting = prompt("Hi there!");
+greeting.search( "Matthew");
+
+
+var wdi = prompt("Nice to meet you. Will you be going to class today?");
+      if (answer == yes) { // true
+            console.log("Excellent! I see you have 9am class, and that you will also be coming from Southwest. Since a bicycle is already your preferred method of travel, lets start planning your route.");
+      }
+
+var bikeLanes = prompt("Will you be using bike lanes or sidewalks?");
+      //if (bikeLanes) = return("You follow the designated bike lanes all the way from SW, obeying most redlights and narrowly avoiding that driver from Virgnia in the silver Civic. You get to Pennsylvaina and find yourself stuck behind several other rider, loosing a few moments in the process."
+
+      // You do however make up for this lost time as you quickly dart through Lafayette Square, past Secret Service, and past the gridlock on K Street as you arrive at class, punctually and without a mark of dirt. You win!");
+
+      //else sidewalks = return("You find yourself riding the sidewalks until coming to 15th street, whereby you narrowly miss crashing into a huge group of school children on a field trip and end up crashing into the Presidents motorcade as it emerges from seemingly nowhere and onto 15th Street. After getting questioned and cleaned up by Secret Service for a half hour, you narrowly avoid detainment. You arrive late to class, flustered and in distress. Too bad - try again tomorrow!");
