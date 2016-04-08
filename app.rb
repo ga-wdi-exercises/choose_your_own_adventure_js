65fb00368c42c4b3cca556d662b6a6601fd0c61d
Matt <mvaden08@gmail.com>
2016-04-07 20:21:36 -0400

    more turning JS into Ruby

--- app.rb
+++ app.rb
@@ -21,7 +21,6 @@ if year.to_i >= 2015
    name = gets.chomp
    puts name
 
-   #`B`iff
    if name == "b"
       puts "Hmm, interesting. Biff is angry and has a cane. Do you stand and fight, or run away like a coward? **(S/R)**"
       action = gets.chomp
@@ -32,8 +31,8 @@ if year.to_i >= 2015
 
       elsif action == "r"
          puts "`R`un like a coward. You get away, but your future son Marty Jr. is heckled for the rest of his days for his dad's cowardice."
-      end
-   #`G`riff
+      end # s/r
+
    elsif name == "g"
       puts "Griff is asking you if you are in, or out. What do you say? **(I/O)**"
       asking = gets.chomp
@@ -44,7 +43,7 @@ if year.to_i >= 2015
 
       elsif asking == "o"
          puts "`O`ut. Good call. You deck Griff in the jaw and run away. He gives chase on his hoverboard and ends up in a pile of manure."
-      end
+      end # i/o
 
    end # b/g
 
@@ -57,16 +56,38 @@ elsif year.to_i >= 1985 && year.to_i <= 2015
 
 elsif year.to_i >= 1955 && year.to_i <= 1984
    puts "I see you're a fan of Back to the Future 1. Your future Mom has just asked you to the Enchantment Under the Sea dance. What do you do? **(Y/N/S)**"
+
+   dance = gets.chomp
+   puts dance
+
+   if dance == "y"
+      puts "Creepy. I hope you have some backup plan in place to get out of this. Until then, you're stuck in 1955."
+
+   elsif dance == "n"
+      puts "Honorable. But this also means that your future Dad will never meet your Mom, and therefore you cannot exist."
+
+   elsif dance == "s"
+      puts "Interesting. You set up an elaborate plan for your future Dad to surprise your Mom by beating you up. Despite going horribly awry, the plan ultimately works. You may go back to your own time."
+
+   end # y n s
 end # 1955 - 1984
 
-#         * `Y`es
-#             * Creepy. I hope you have some backup plan in place to get out of this. Until then, you're stuck in 1955.
-#         * `N`o
-#             * Honorable. But this also means that your future Dad will never meet your Mom, and therefore you cannot exist.
-#         * `S`et her up with George
-#             * Interesting. You set up an elaborate plan for your future Dad to surprise your Mom by beating you up. Despite going horribly awry, the plan ultimately works. You may go back to your own time.
+elsif year.to_i < 1955
+   puts "I see you're a fan of Back to the Future 3. You've run out of nitroglycerin to get back to your own time. How do you power the Time Machine? **(H/M/T)."
+
+   power = gets.chomp
+   puts power
+
+   if power = "h"
+      puts "Good idea, but no. The time machine needs to get to 88mph. 12 horsepower ain't gonna cut it."
+
+   elsif power = "m"
+      puts "You'd be better off drinking the moonshine. Do not pass Go, do not collect $200. Stuck in 1855."
+end
+
+
 # * `< 1955`
-#     * I see you're a fan of Back to the Future 3. You've run out of nitroglycerin to get back to your own time. How do you power the Time Machine? **(H/M/T)**
+#
 #         * `H`orses
 #             * Good idea, but no. The time machine needs to get to 88mph. 12 horsepower ain't gonna cut it.
 #         * `M`oonshine
