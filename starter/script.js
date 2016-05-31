4609d2adb8106cf688b0f5b8e2575e86f4b58578
James <james@team-wombat.com>
2016-05-31 14:29:44 -0400

    Began js prompts and alerts

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,11 @@
 // put code here!
+
+var name = prompt('Now Playing: The Matrix Re-Reloaded! Starring:','Enter your name');
+
+alert("I would've stuck with Keanu Reeves, but I'm not the director, you are!");
+
+alert("You wake up after a late night of haxing. Suddenly you notice that someone has taken control of your computer!");
+
+alert("Follow the white rabbit, "+name+".");
+
+alert("Then--a knock at the door! A white rabbit tattoo!");
