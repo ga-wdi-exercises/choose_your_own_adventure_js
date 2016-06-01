4a338df203be03c65bc7b71b7ad8983145d9ed41
Andrew Young <prince.of.utah@gmail.com>
2016-06-01 19:23:33 -0400

    Gandy Choose Your Own Advernture HW

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,85 @@
-// put code here!
+var alcohol = prompt("Hey, what's your poison?");
+console.log("So I hear you like " + alcohol);
+
+var bar = prompt("What bar should we go to tonight? Right Proper, Boundary Stone, or Shaw Tavern? **(R/B/S)**");
+
+if (bar == "R") {
+  console.log("So Right Proper it is then. Do you want Whiskey or Beer? **(W/B)**");
+
+  var whiskeyOrBeer = prompt("Do you want Whiskey or Beer? **(W/B)**");
+
+  if (whiskeyOrBeer == "B") {
+    console.log("Alright Beer it is. This place is fun but you have work tomorrow. Would you like to stay or leave? **(S/L)**");
+
+    var stay = prompt("Would you like to stay or leave? **(S/L)**");
+
+      if (stay == "S") {
+        console.log("You stayed and had a few more beers and then stayed a little too late. You met a few late night weirdos but everything turned out fine.");
+      } else if (leave == "L") {
+        console.log("You leave and get a good nights rest and end up doing very well on a surprise work evaluation the next day. Good for you!");
+      }
+
+  } else if (whiskeyOrBeer == "W") {
+    console.log("Awesome. Let's get you some firewater. Hmm you might be a little drunk. Would you like to stay or leave? **(S/L)**");
+
+    var stay = prompt("Would you like to stay or leave? **(S/L)**");
+
+      if (stay == "S") {
+        console.log("You stayed and had a few more beers and then stayed a little too late. You met a few late night weirdos but everything turned out fine.");
+      } else if (leave == "L") {
+        console.log("You leave and get a good nights rest and end up doing very well on a surprise work evaluation the next day. Good for you!");
+      }
+
+else if (bar == "B") {
+        console.log("Cool choice. Boundary Stone is great. Do you want Whiskey or Beer? **(W/B)**");
+
+        var whiskeyOrBeer = prompt("Do you want Whiskey or Beer? **(W/B)**");
+
+        if (whiskeyOrBeer == "B") {
+          console.log("Alright Beer it is. This place is fun but you have work tomorrow. Would you like to stay or leave? **(S/L)**");
+
+          var stay = prompt("Would you like to stay or leave? **(S/L)**");
+
+            if (stay == "S") {
+              console.log("You stayed and had a few more beers and then stayed a little too late. You met a few late night weirdos but everything turned out fine.");
+            } else if (leave == "L") {
+              console.log("You leave and get a good nights rest and end up doing very well on a surprise work evaluation the next day. Good for you!");
+            }
+
+        } else if (whiskeyOrBeer == "W") {
+          console.log("Awesome. Let's get you some firewater. Hmm you might be a little drunk. Would you like to stay or leave? **(S/L)**");
+
+          var stay = prompt("Would you like to stay or leave? **(S/L)**");
+
+            if (stay == "S") {
+              console.log("You stayed and had a few more beers and then stayed a little too late. You met a few late night weirdos but everything turned out fine.");
+            } else if (leave == "L") {
+              console.log("You leave and get a good nights rest and end up doing very well on a surprise work evaluation the next day. Good for you!");
+            }
+
+if (bar == "S") {
+              console.log("Terrible choice. Shaw Tavern is the worst. Do you want Whiskey or Beer? **(W/B)**");
+
+              var whiskeyOrBeer = prompt("Do you want Whiskey or Beer? **(W/B)**");
+
+              if (whiskeyOrBeer == "B") {
+                console.log("Alright Beer it is. This place is fun but you have work tomorrow. Would you like to stay or leave? **(S/L)**");
+
+                var stay = prompt("Would you like to stay or leave? **(S/L)**");
+
+                  if (stay == "S") {
+                    console.log("You stayed and had a few more beers and then stayed a little too late. You met a few late night weirdos but everything turned out fine.");
+                  } else if (leave == "L") {
+                    console.log("You leave and get a good nights rest and end up doing very well on a surprise work evaluation the next day. Good for you!");
+                  }
+
+              } else if (whiskeyOrBeer == "W") {
+                console.log("Awesome. Let's get you some firewater. Hmm you might be a little drunk. Would you like to stay or leave? **(S/L)**");
+
+                var stay = prompt("Would you like to stay or leave? **(S/L)**");
+
+                  if (stay == "S") {
+                    console.log("You stayed and had a few more beers and then stayed a little too late. You met a few late night weirdos but everything turned out fine.");
+                  } else if (leave == "L") {
+                    console.log("You leave and get a good nights rest and end up doing very well on a surprise work evaluation the next day. Good for you!");
+                  }
