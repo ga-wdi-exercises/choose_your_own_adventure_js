815ee67314315a0ac0ef19665aada4b08ada89ee
Tyler Rosenberg <tyryanr@gmail.com>
2016-07-04 14:09:37 -0400

    initial commit

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,13 @@
-// put code here!
+var name = prompt("Please enter your name");
+var age = parseInt(prompt("Please enter your age"));
+
+if (age < 16) {
+  alert("You must be at least 16 to attend sports games alone, sorry.");
+} else {
+
+var account = parseInt(prompt("How much money do you have in your pocket? Choose a number between 300 and 800"));
+
+var sportsChoice = "Please choose one sport you would like to go watch today at the stadium. Your options are basketball, baseball, or football");
+
+
+}
