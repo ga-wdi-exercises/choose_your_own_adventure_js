65019df291e926fabd88f2fe34a49b68c7b92eb0
Christopher Phillips <cphillips918@gmail.com>
2016-07-03 23:03:27 -0400

    finishing some functionality in the js

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,7 @@
 var images = document.getElementById("images");
 var text = document.getElementById("text");
-var input = document.getElementById('input');
 var buttonBox = document.getElementById('buttonBox');
+var input = document.getElementById('box')
 var candidate;
 
 function changeText(words) {
@@ -15,21 +15,40 @@ function changeImage(img) {
 function changeButtons(buttonList) {
     buttonBox.innerHTML = "";
     for (var i = 0; i < buttonList.length; i++) {
-        buttonBox.innerHTML += "<button     onClick="+buttonList[i][1]+">" + buttonList[i][0] +     "</button>";
+        buttonBox.innerHTML += "<button onClick="+buttonList[i][1]+">" + buttonList[i][0] + "</button>";
     };
 }
 
-function changeInput(inputVar) {
+function hideButtons(){
+    document.getElementById("buttonBox").style.visibility = "hidden";
+}
 
+function showButtons(){
+    document.getElementById("buttonBox").style.visibility = "visible";
+}
+
+function hideInput(){
+    document.getElementById('box').style.visibility = "hidden";
+}
+
+function showInput(){
+    document.getElementById('box').style.visibility = "visible";
 }
 
 function changeScene(scenario) {
     changeImage(scenario.image)
     changeText(scenario.text)
     changeButtons(scenario.buttons)
-    changeInput(scenario.input)
+    showButtons();
+    hideInput();
 };
 
+function chageSceneInput(scenario){
+    changeImage(scenario.image)
+    changeText(scenario.text)
+    hideButtons();
+    showInput();
+}
 
 scenario = {}
 var scenario = {
@@ -47,8 +66,8 @@ var scenario = {
     },
     three: {
         image: "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg",
-        text: "Oh... your with the Donald",
-        buttons: [],
+        text: "Oh... your with the Donald. Well...Where do we start then?",
+        buttons: [["Take the Donald Pledge", "changeScene(scenario.nine)"],["","changeScene(scenario.ten)"]],
         input:[],
     },
     four: {
@@ -60,21 +79,56 @@ var scenario = {
     five: {
         image: "http://3ynk0w1orym5czc542xmo55k.wpengine.netdna-cdn.com/wp-content/uploads/2016/01/FBI.jpg",
         text: "WHEOWHEOWHEOWHEO You just took the fall for the email scandal. Consider your career in politics over.",
-        buttons: [],
+        buttons: [["Go Back to Start","changeScene(scenario.one)"]],
         input:[],
     },
     six:{
         image: "http://i.huffpost.com/gen/1439544/images/o-BILL-CLINTON-OBAMA-facebook.jpg",
         text: "Ha! That was a joke. Bill's a little busy cleaning up some Clinton Foundation loose ends. Let's get you started on some operations. Do you want to work on the Twitter account or the Reddit account?",
-        buttons: [["Twitter", "changeScene(scenario.seven)"],["Reddit","changeScene(scenario.eight)"]],
+        buttons: [["Twitter", "chageSceneInput(scenario.seven)"],["Reddit","changeScene(scenario.eight)"]],
         input:[],
     },
     seven:{
         image: "http://urbanintellectuals.com/wp-content/uploads/2015/07/clinton-and-obama.jpg",
         text: "Nice! Just keep interacting with all of our bought followers to keep the activity up. How many more do you think we should buy from India to keep us trending?",
+        buttons: [[ , ]],
+        input:[],
+    },
+    eight:{
+        image: "https://i.ytimg.com/vi/keWZk9-Z2gg/maxresdefault.jpg",
+        text: "Looks like we didn't buy enough followers to stay relevant. The SS Hillary has sunk and you're out of the job.",
+        buttons: [],
+        input:[],
+    },
+    nine:{
+        image: "",
+        text: "",
+        buttons: [],
+        input:[],
+    },
+    ten:{
+        image: "",
+        text: "",
+        buttons: [],
+        input:[],
+    },
+    eleven:{
+        image: "",
+        text: "",
+        buttons: [],
+        input:[],
+    },
+    twelve{
+        image: "",
+        text: "",
         buttons: [],
         input:[],
     }
 };
-
 changeScene(scenario.one);
+
+input.onkeypress = function(event) {
+  if (event.key == "Enter") {
+      changeScene(scenario.eight)
+  }
+};
