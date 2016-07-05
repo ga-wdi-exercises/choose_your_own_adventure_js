a8fa463ab7d4479101b6b3cd7923ac4b78aee5d6
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-05 00:52:22 -0400

    Completed Adventure Game HW

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,34 @@
-// put code here!
+confirm("This game contains strong language and some adult situations. Player discretion is advised...")
+
+confirm("Welcome to West Baltimore");
+
+var response = prompt("Wanna be in the game shorty? Yes or No?");
+
+if (response === "No") {
+  alert("You can't lose if you don't play. [Technical, but less fun, victory]");
+  //how do you fully stop code from running at this point?
+} else {
+  alert("Ight young hoppa, I'll put you in charge of the count.");
+};
+
+var count = prompt("It's the end of the day shorty. Is the COUNT RIGHT or did you FUCK UP THE COUNT?");
+
+if (count === "COUNT RIGHT") {
+  alert("You're doing well in the pitt. Keep this up and you'll get points on the package! Just make sure to be on the lookout for Omar...");
+} else if (count === "FUCK UP THE COUNT") {
+  alert("Nobody steals from Avon Barksdale! Weebay. Stinkum. Fuck him up!");
+};
+
+alert("OMAR COMIN'! OMAR COMIN'!")
+alert("Omar wants to rob the stash. You can either tell him the truth (KITCHEN), lie (NO REUP), or FIGHT.")
+
+var whereitat = prompt("Omar: 'Where it at shorty?''")
+
+if (whereitat = "KITCHEN") {
+    alert("Omar: Oh, indeed.");
+  }
+  // // } else if (whereitat === "NO REUP") {
+  //   alert ("That's a raggedy ass play, boi. I know the stash is here. Can't lie to Omar. *shotgun goes off* You lose the game")
+
+  // Need to complete story, add loop, and a number value.
+  // Also, seeing issues with paths. Paths do not end the game, they just move on to the next level. But for the most part I have a good understanding. Please help with these! Thank you! :)
