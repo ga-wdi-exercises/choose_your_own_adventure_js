dc680bec0ea249430a7571acd2971c973b541f98
michaelmahony <michael.mahony@umbc.edu>
2016-07-05 10:00:48 -0400

    Added another scene.

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,5 @@
 // put code here!
+console.log("Running js");
 var situations = {
   start : {
     // The main text prompt of the scene
@@ -35,23 +36,58 @@ var situations = {
     end: false,
   },
 
-  council : {
+  pub : {
+    text: "The pub. Oh dear, the pub. God, what a terrible hangover. " +
+    "You vaguely remember being angry, angry about something " +
+    "that seemed important. You had been telling people about it at great length. Something " +
+    "about a bypass that you had just found out about, although it had been in the works for months. " +
+    "The word yellow wandered through your mind again in search of something to connect with.",
 
+
+    suggestions: ['yellow'],
+
+    objects : {},
+
+    connected_scenes : ['yellow'],
+
+    end : false,
   },
 
-  }
+  yellow : {
+    text: "Fifteen seconds later you are out of the house and lying in front of a big yellow " +
+    "bulldozer that is advancing up your garden path. The driver shouts down, 'Come off it! " +
+    "You can't win, you know. You can't lie in front of the bulldozer indefinitely.'",
+
+    suggestions: ['taunt', 'reason'],
+
+    objects : {
+      taunt: "'I'm game', you say, 'we'll see who rusts first.' \n" +
+      "'I'm afraid you'll have to accept it,' the driver said. 'It's a bypass. You've got to " +
+      "build bypasses.'",
+
+      reason: "'You were quite entitled to make suggestions at the approprate time', " +
+      "the driver shouted.\n" +
+      "'Appropriate time?!' you hoot. 'Appropriate time? The first I knew about it was when a " +
+      "workman arrived at my home yesterday. I asked him if he'd come to clean the windows and " +
+      "he said no he'd come to demolish the house. He didn't tell me straight away of course. Oh no. " +
+      "First he wiped a couple of windows and charged me a fiver. Then he told me.'",
+    },
+    connected_scenes : [],
+    end : false,
+  },
 }
 
+console.log("Got to end of scene declaration");
 
 var user = {
   current_scene : 'start',
   scene_info : situations.start,
-  current_text : situations.start.text + "\n\n" + situations.start.suggestions,
+  current_text : situations.start.text + "\n\n" + "Options: " + situations.start.suggestions,
 
   set_new_scene : function(scene_name) {
     user.current_scene = scene_name;
     user.scene_info = situations[scene_name];
-    user.current_text = user.scene_info.text + "\n\n" + user.scene_info.suggestions;
+    user.current_text = user.scene_info.text + "\n\n" + "Options: " + user.scene_info.suggestions;
   }
 }
 
@@ -75,3 +111,5 @@ function main() {
     }
   }
 }
+
+main();
