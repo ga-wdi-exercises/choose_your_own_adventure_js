5cc127b3c9500050c3c755a8ecdab1ae18f16cbe
Domonique Griffin <griffind2009@gmail.com>
2016-09-14 17:35:41 -0400

    First few prompts

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,43 @@
-// put code here!
+var age = 0
+var location = ""
+confirm("Ready to play?");
+
+var age = prompt("Please enter you age")
+if (age >= 18) {
+  document.write("Welcome to Treasure Island");
+}
+else {"Come back in a few years."
+}
+
+var location= prompt("Do you choose cave one, cave two, or cave three?")
+
+/*if (cave one) {
+  document.write("Unfortunately you fell into a gian swarm swarming with alligators.")
+  prompt(Do you give up and let the gators take you?)
+}
+
+/*function myCave() {
+  cave 1 or cave 2 or cave 3;
+  console.log()
+}
+myCave()
+if (current == 0 && myCave == cave 1) {
+            document.write("Lucky you the alligators were nice and decided ot let you live.")*/
+
+/*var options= {
+  cave one: function(story) {
+  return Awesome Choice! Unfortunately you fell into a giant swamp, swarming with alligators.
+},
+  cave two: function(story) {
+    return
+  },
+  cave three: function(story) {
+    return
+  },
+};*/
+/*myCave() {
+  if (location === cave one) {
+  document.write("Awesome Choice! Unfortunately you fell into a giant swamp, swarming with alligators.")
+}
+}
+myCave()*/
