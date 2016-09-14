e0fd5b6ff0907eb0df85bbbacdbad73a7cf30f14
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-13 23:46:18 -0400

    started

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,11 @@
 // put code here!
+var name = prompt("Welcome! What is your name?");
+var ans = prompt("Hi " + name + "!"+ "Welcome aboard Oceanic Flight 815! Is this your first time with us? ");
+if (ans === "yes") {
+  alert("Welcome back!");
+} else {
+  alert("Thanks for choosing Oceanic Flight! Please sit back and enjoy the flight!")
+ }
+
+
+// alert("omg plane crashing!");
