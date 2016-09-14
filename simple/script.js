762a80864ce9f7f531cd869ee9c5eadeee2ba7e8
Tim Foley <thetimfoley@gmail.com>
2016-09-14 19:26:36 -0400

    finished BttF3

--- simple/script.js
+++ simple/script.js
@@ -58,6 +58,24 @@ if (!toPlayOrNotToPlay) {
       }
 
   } else if (response < 1955) {
+    response = prompt("I see you're a fan of Back to the Future 3. You've run out of nitroglycerin to get back to your own time. How do you power the Time Machine? ([H]orses/[M]oonshine/[T]rain)")
+
+    if (response == "H") {
+      alert("Good idea, but no. The time machine needs to get to 88mph. 12 horsepower ain't gonna cut it.")
+    } else if (response == "M") {
+      alert("You'd be better off drinking the moonshine. Do not pass Go, do not collect $200. Stuck in 1855.")
+    } else if (response == "T") {
+      response = prompt("Good call! This plan seems to be working. But wait! Clara wants to go Back to the Future with you at the last moment. What do you do? ([T]ake her/[L]eave her)")
+
+      if (response == "T") {
+        alert("Interesting choice. Unfortunately the Doc can't grab Clara and get back to the car in time. He ends up staying in 1855 with her.")
+      } else if (response == "L") {
+        alert("Smart choice. Unfortunately the Doc was deeply in love with Clara, and when he gets back to 1985 he becomes very depressed.");
+      } else {
+        huh();
+      }
+    }
+
 
   } else {
     huh();
