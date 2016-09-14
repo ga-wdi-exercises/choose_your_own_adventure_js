126fef5676388ecc424121eb3a31309967900b29
Barrett Cope <barrettcope@gmail.com>
2016-09-13 20:56:18 -0400

    Last commit before methods

--- starter/script.js
+++ starter/script.js
@@ -15,29 +15,48 @@ Go ask for help.
 */
 var story = {
   start: function() {
-    // not finished
+    console.log("not finished");
   },
   stats: {
+    none: 1,
     luck: 1,
     strength: 1,
-    hubris: 0,
-    hp: 10,
-    increaseStat: function() {
-      story.stat.current += 0.1;
+    charisma: 1,
+    increaseStat: function(statName) {
+      story.stats[statName] += 0.1;
     },
-    decreaseStat: function() {
-      story.stat.current -= 0.1;
+    decreaseStat: function(statName) {
+      story.stats[statName] -= 0.1;
     }
   },
-  characterName: undefined,
-  display
-}
+  scenes: [],
+  /* scene template
+  scene: {
+    id: <int>,
+    final: [true|false],
+    setting: "text",
+    options: [array of option objects],
+  },*/
+  /* option template
+  option: {
+    id: <int>,
+    choiceValue: "text",
+    hidden: [true|false],
+    statModifier: "name of story.stats[n]", // use "none" for no modification
+    difficulty: [<int difficulty ratio numberator>,<int difficulty ratio denominator>], // use [0,1] for automatic success
+    successText: "text",
+    successConsequences: function() {
 
+    },
+    successSceneId: <int>,
+    failureText: "text",
+    failureConsequences: function() {
 
-function d20() {
-  return Math.floor(Math.random() * (20 - 1 + 1)) + 1;
-}
-
-function dieRoll(max) {
-  return Math.floor(Math.random() * (max - 1 + 1)) + 1;
+    },
+    failureSceneId: <int>
+  }
+  */
+  d: function(max) {
+    return Math.floor(Math.random() * (max - 1 + 1)) + 1;
+  },
 }
