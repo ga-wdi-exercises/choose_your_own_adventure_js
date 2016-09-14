90d7e7316b4ea01496f9c95eb3b0b0bbb14aaa8e
RYANBROPHY <BROPHY2321@ME.COM>
2016-09-14 14:23:56 -0400

    halfway

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,20 @@
-// put code here!
+var start = confirm("Are you ready to enter the Temple of Doom?")
+
+var choice = prompt("Will you take the Golden Monkey or run?");
+  if (choice=== "take"){
+    console.log("taken")
+  } else if(choice==="run"){
+    console.log("running")
+  }
+  var run = prompt("Are you scared");
+    if (run=== "yes"){
+      console.log("scared")
+    } else if(run==="no"){
+      console.log("not scared")
+    }
+    var before = prompt ("Have you ever run for your life?");
+      if (before==="yes"){
+        console.log("runrun")
+      } else if (before==="no"){
+        console.log("nono")
+      }
