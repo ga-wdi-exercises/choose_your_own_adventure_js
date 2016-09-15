6ecec5631a82f99acb2bee1c7103a99ba3bf7903
Latoya Watson <latoyamwatson@gmail.com>
2016-09-14 20:56:42 -0400

    for loops, functions, basic game works as expected

--- starter/script.js
+++ starter/script.js
@@ -1,24 +1,17 @@
-// choose your own adventure:
-// function
-//  if
-//  else
-//  switch
-//  case
-//  break
-// put code here!
-// each scene should have it's own function
-
 var name = prompt("Hello! What is your name?");
 
-var pokemon = prompt("Hello, " + name + "." + " Quick! You see a Pikachu and A Charmander! Which one do you want to battle? Choose: [p] for Pickachu or [c] for Charmander?");
+var pokemon = prompt("Hello, " + name + "." + " Quick! You see a Pikachu, a Charmander, and a Squirtle! Which one do you want to battle? Choose: [p] for Pikachu, [c] for Charmander, [s] for Squirtle");
   if (pokemon === "p") {
-  var p = prompt("Awesome, you got a Pikachu! Ready for a gym battle? Do you want to battle Brock or Misty?" );
+    var p = prompt("Awesome, you got a Pikachu! Ready for a gym battle? Do you want to battle Brock or Misty?" );
 pikachu();
-// call the function, here for Pikachu
 
 } else if (pokemon === "c") {
     var c = prompt("Sweet, man! a Charmander! Ready for a gym battle? Do you want to battle Erika or Misty?");
 charmander();
+
+} else if (pokemon === "s") {
+    var s = confirm("That's cool. You picked Squirtle. Oh crap, it's Team Rocket! Want to battle them?");
+teamRocket();
 
 } else {
     alert("No, you aren't following the rules!!");
@@ -26,18 +19,45 @@ charmander();
 
 function pikachu() {
   if (p === "Brock") {
-    prompt("You lost, bro. Brock rocked you. Back to the Pokemon Center");
+    var win = confirm("You lost, bro. Brock rocked you. Back to the Pokemon Center");
+    PokemonCenter();
 } else if (p === "Misty") {
-    prompt("Lol. That wasn't even a fair fight. Owned");
+    var lose = confirm("Lol. That wasn't even a fair fight. Owned. Oh crap, it's TEAM ROCKET!!! WANNA BATTLE THEM?!");
+  teamRocket();
   }
 }
 
 function charmander() {
   if (c === "Erika") {
-    confirm("You beasted her! Nice Job!! Oh no, It's TEAM ROCKET. Should we battle them?");
+    var win = confirm("You beasted her! Nice Job!! Oh no, It's TEAM ROCKET. Should we battle them?");
+  teamRocket();
+
 } else if (c === "Misty") {
-    confirm("Whoa, she killed you! Go back to the Pokemon Center!");
+    var lose = confirm("Whoa, she killed you! Go back to the Pokemon Center!");
+    PokemonCenter();
   }
 }
 
+function teamRocket() {
+  if (s === true) {
+    var win = confirm("Haha, TEAM ROCKETS blasting off again! Okay, let's go to the Pokemon Center!");
+    PokemonCenter();
+
+  } else {
+    var lose = confirm("Welp, TEAM ROCKET doesn't care if you don't want to fight! So they took all your Pokemon and you fainted. Game over.");
+    console.log("Game over.");
+  }
+}
+
+function PokemonCenter() {
+  var welcome = confirm("Welcome to the Pokemon Center! We can heal your Pokemon back to perfect health! Shall we heal your Pokemon?");
+    if (welcome === true) {
+      confirm("Okay! We'll need your Pokemon! .  .  .  .  .   .   .   .   .     . Thank you! Your Pokemon are fully healed. We hope to see you again.");
+    } else {
+      alert("You don't want our help? Well, game over.");
+      console.log("Game over.");
+    }
+}
+
+// switch ()
 // you wanna play again? while loop. or switch
