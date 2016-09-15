2955f47e1b4997e402e969eed532456e536d30eb
Latoya Watson <latoyamwatson@gmail.com>
2016-09-14 21:31:17 -0400

    Fixed a couple of bugs, added or statements

--- starter/script.js
+++ starter/script.js
@@ -1,15 +1,15 @@
 var name = prompt("Hello! What is your name?");
 
 var pokemon = prompt("Hello, " + name + "." + " Quick! You see a Pikachu, a Charmander, and a Squirtle! Which one do you want to battle?\n\n Choose: [p] for Pikachu, [c] for Charmander, [s] for Squirtle");
-  if (pokemon === "p") {
+  if (pokemon === "p" || pokemon === "Pikachu") {
     var p = prompt("Awesome, you got a Pikachu! Ready for a gym battle? Do you want to battle Brock or Misty?" );
 pikachu();
 
-} else if (pokemon === "c") {
+} else if (pokemon === "c" || pokemon === "Charmander") {
     var c = prompt("Sweet, man! a Charmander! Ready for a gym battle? Do you want to battle Erika or Misty?");
 charmander();
 
-} else if (pokemon === "s") {
+} else if (pokemon === "s" || pokemon === "Squirtle") {
     var s = confirm("That's cool. You picked Squirtle. Oh crap, it's Team Rocket! Want to battle them?");
 teamRocket();
 
@@ -18,21 +18,21 @@ teamRocket();
 }
 
 function pikachu() {
-  if (p === "Brock") {
+  if (p === "Brock" || p === "brock") {
     var win = confirm("You lost, bro. Brock rocked you. Back to the Pokemon Center");
     PokemonCenter();
-} else if (p === "Misty") {
+} else if (p === "Misty" || p === "misty") {
     var lose = confirm("Lol. That wasn't even a fair fight. Owned. Oh crap, it's TEAM ROCKET!!! WANNA BATTLE THEM?!");
   teamRocket();
   }
 }
 
 function charmander() {
-  if (c === "Erika") {
+  if (c === "Erika" || c === "erika") {
     var win = confirm("You beasted her! Nice Job!! Oh no, It's TEAM ROCKET. Should we battle them?");
   teamRocket();
 
-} else if (c === "Misty") {
+} else if (c === "Misty" || c === "misty") {
     var lose = confirm("Whoa, she killed you! Go back to the Pokemon Center!");
     PokemonCenter();
   }
