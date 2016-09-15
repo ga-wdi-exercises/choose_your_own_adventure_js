b0ec3c5961438f75514409d529b7027e88b8842d
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-14 21:19:31 -0400

    final but couldn't get alert to show

--- starter/script.js
+++ starter/script.js
@@ -18,7 +18,7 @@ alert("I want to play a game. This time I will make it easy.")
 var one =  prompt("You will be given fruits to choose. Choosing the right one will keep you alive. Okay?");
   while (one ==="no"){
      one=prompt("You have no choice. Enter 'yes' or you go through the hard way");
-     if (one === "yes") {
+     if (one === "ok") {
        prompt("Let's do it.")
      }
    }
@@ -28,29 +28,45 @@ var two = prompt("Pick a fruit. Mango (m) ,Apple (a), or Banana (b)");
   if (two === "m"){
      m()}
   else if (two === "a"){
-    a()
+    a()}
+  else if (two === "b"){
+    b()
   }
 
 function m() { prompt("You're still alive, but still too early to say it. You want Irwin (i) or Edward (e)?")};
 if (m === "i"){
- prompt("I'm sorry. You need to swallow it")
+alert("I'm sorry. You need to swallow it")
  }
 else if (m === "e"){
-  prompt("I like Edward mango too! I'll let you live")};
+  alert("I like Edward mango too! I'll let you live")};
 
 
 function a() {prompt(" I like apples too. Pick one of them. Granny Smith (g), Pink Lady (p), or Fuji (f)?")};
 if (a === "g"){
-  prompt("I'm sorry. This will be the last food of your life. Don't you know that Snow White chose the green apple too?")
+  alert("I'm sorry. This will be the last food of your life. Don't you know that Snow White chose the green apple too?")
 }
 else if (a==="p"){
-  prompt("Congratulations! Pink Lady just saved your life. You can eat it and go home.")
+  alert("Congratulations! Pink Lady just saved your life. You can go home.")
 }
 else if (a ==="f"){
-  prompt("Hahaha. You get a free trip to Japan!")
+  alert("Hahaha. You get a free trip to Japan!But don't ever come back again!")
 }
 
+function b(){
+  alert("Do you want Plaintain (p) or Red (r)?")
+};
+switch(b){
+  case "p":
+  alert("You're free! I was just playing with you")
+  break;
+  case "r":
+  alert("Choosing banana is safe. I like minions")
+  break;
+}
 
+// p:function(plaintain){
+//   prompt("This plaintain is frozen.")
+// }
 
 // switch (two){
 //     case "m":
