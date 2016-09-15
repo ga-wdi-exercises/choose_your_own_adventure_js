a514c380fbfefe8af043b628c9ef4b34112361c9
Chris Jaffier <chrisd@Chriss-MBP.home>
2016-09-15 00:19:26 -0400

    if statements made

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,22 @@
-// put code here!
+var name = 0;
+name = prompt("What is your name?")
+document.write("Welcome to the wasteland" + " " + name)
+firstChoice()
+
+function firstChoice () {
+  var choice1 = 0
+  choice1 = prompt("Are you heading north or south?")
+
+  if ("north") {
+  document.write("You meet a donkey")
+}
+  if ("south") {
+    document.write("You meet a Gecko")
+    secondChoice()
+  }
+}
+
+function secondChoice() {
+  document.write("The End")
+
+}
