53a0abc7070a4ca22d955af065e35f597f1075a8
William Keating <keating49@gmail.com>
2016-09-15 08:54:47 -0400

    adventurehw

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,35 @@
 // put code here!
+var yards = i;
+
+confirm("Are you ready for some football!?")
+var team = prompt("What's your favorite team?");
+  if (team == "Redskins") {
+    console.log("You are cool")
+  } else {
+    console.log("Better not be the Cowboys")
+  }
+  console.log("Welcome to Monday Night Football! Tonight the Redskins play the Cowboys");
+var coinToss = prompt("Heads or Tails?")
+  if (coinToss == "Heads") {
+    console.log("You have won the toss!")
+  } else {
+    console.log("You lost the toss!")
+  }
+var result = prompt("Did you win the toss?");
+  if (result == "Yes") {
+    console.log("Congrats! You have choice on if you want the ball first or second half")
+  } else {
+    console.log("The other team has choice")
+  }
+ prompt("The Cowboys kick the ball to the Redskins")
+ prompt("How many yards did the Redskins get on kickoff")
+
+ for (var i = 0; 0 < 100; i++) {
+  if (yards = 100) {
+    console.log("touchdown")
+} else if (yards >= 50) {
+  console.log("nice return")
+} else {
+  console.log("better luck next time!")
+ }
+}
