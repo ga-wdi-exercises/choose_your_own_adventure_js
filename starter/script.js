0986a7d6a6b832c61f25998b75eb13ee6f7a98a9
manish pradhan <pradhanmanish33@gmail.com>
2016-09-15 00:21:47 -0400

    initial commit

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,55 @@
-// put code here!
+var greet = alert("Welcome to Top chef! How are you doing today?");
+
+var time = 0;
+  for (var i = 1; i < time.length; i ++){
+    time = time + i ;
+}
+  //first condition over cooked meal
+var input = function (){
+  input = parseInt();
+}
+    if ('ok') {
+     prompt('oh! looks like it\'s over cooked ! serve it? yes or no');}
+  else if (input =='yes') {
+      prompt ("Wow ! new dish, would you share the receipe? ya or na");}
+     else if (input =='ya') {
+        prompt ('cool! thanks') ;}
+      else if (input =='na') {
+          prompt ('Do you want to win? yup or nope.') ;}
+        else if (input =='yup') {
+            prompt ('You recieve a secret ingredient') ;}
+           else if (input =='nope') {
+              prompt ('hmmm.. well, can not force you') ;}
+     else if (input =='no') {
+        prompt('you have nothing to serve, you lose!') ;}
+        else 'I did not get it! Please be clear!';
+
+
+  //second condition
+ if ('minute left 15'){
+    prompt ('you got few minutes left on the clock. Do you need more minutes? like 5 0r 10') ;}
+     else if (time =='10'){
+    prompt ('that is too much. can you make dish? maybe 1 or 2');}
+   else if (time =='1'){
+      prompt ('hmmm.. you are slow !') ;}
+     else if (time =='2'){
+        prompt ('wow! you are the fastest cook ever.') ;}
+     else if (time =='5'){
+    prompt ('little more but still good. finish the dish') ;}
+    else 'Your choice !';
+
+
+
+  //third condition
+ if ('mean judges'){
+    prompt ('you are sweating and really nervous. do you present the dish, ola or adios.') ;}
+   else if (input =='ola'){
+      prompt ('judges look it strangely, did you cook it first time or second time? primero or segundo') ;}
+
+     else if (input =='primero'){
+        prompt ('no wonder it taste so different, still good job') ;}
+       else if (input =='segundo'){
+          prompt ('still no improvement, but still did a good job') ;}
+        else  if (input =='adios'){
+            prompt ('there is no place for you here, go home.') ;}
+            else 'You can do whatever you want!';
