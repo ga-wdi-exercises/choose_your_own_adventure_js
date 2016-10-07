1b1722124dd4f22f4813f1209220cbbdab13084e
Robert Zehnder <robzcodes@gmail.com>
2016-10-07 14:38:57 -0400

    Adds function to capture initial inputs and give user initial prompt

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,11 @@
 // put code here!
+
+
+function submission () {
+  var name = document.getElementById('nameBox').value;
+  var state = document.getElementById('ddMenu').value;
+
+  prompt("Hello " + name + "! You chose " + state + ". Enter the number associated with option you would like to select. (Ex. '2')", "Enter Option");
+
+  
+}
