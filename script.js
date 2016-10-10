5fd2d16fe7aeedc7e35fabca70aaef516b8d9b54
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2016-10-09 23:22:14 -0400

    opening prompts

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('script is running');
+
+alert('Welcome to RoseWood, can you keep a Secret?');
+
+var Liars = prompt("Help us find expose 'A' before it's too late! Type 'ssh' to continue!");
+  if ('ssh'){
+    console.log('Enter');
+  }
+
+alert('A' + '  '+ 'has discovered you\'re helping the liars and has uncovered a secret about you!');
+
+var A = prompt("I know what you did last summer, don't worry, I'm saving your later!" + "Type Continue to help Liars or goodbye to leave game!");
+ if ("continue"){
+   console.log("continue");
+ }
