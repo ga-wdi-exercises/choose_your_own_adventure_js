63af19eb819bf767db0f8f2f0d88c1d25c35289f
“SandyPok” <Pok.Sandy@yahoo.com>
2016-11-16 14:06:37 -0500

    This is my homework from 11/15/2016.

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,11 @@
 // put code here!
+
+alert("Startup of my game");
+
+alert("This is a story about a girl who loves cheese");
+
+let character = prompt("Her name is:_____", undefined);
+
+if (character === "Yunaleska"){
+  charater = prompt ("ENTER NAME");
+}
