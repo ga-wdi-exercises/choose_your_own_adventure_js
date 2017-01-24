d42e082ea0f208cffab6875aaef6936b5977411c
Collin OConnell <ococncol@gmail.com>
2017-01-24 16:23:15 -0500

    added psudocode

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,50 @@
 // put code here!
+//ask for name
+//Welcome to Dark Souls username
+
+//choose your class
+//Knight, Theif, Pyromancer
+
+//choose Knight
+//What weapon would you like? Sword and Sheild, Axe, Nothing(MAN-UP)
+  //Choose Sword and Sheild
+    //Choose your path
+      //Left
+      //You enter a clearing
+          //There is a chest in the middle of the clearing, what do you want to do.
+            //Walk right up and open it.
+              //Its a Mimik it gets up and round houses you in the face and You Died.
+
+            //Stay as far away
+              //You hug the wall and get to the other end of the clearing
+                //You open the door at the other end of the clearing and enter a hall
+                //with two doors and a hole in the middle
+                  //Left Door
+                    //Open the door and a dog jumps out and kills you. You Died.
+
+                  //Right Door
+                    //Open the door and a dog jumps out and kills you. You Died
+
+                  //Fall Down hole
+                    //You Take Fall Damage, and the Boss Battle begins
+
+
+
+
+            //Do Nothing
+              //Spear comes out of the wall and You Died.
+
+      //Right
+      //Fell off cliff. You Died.
+
+      //Straight
+      //Pitfall onto Spikes. You Died.
+
+
+//Bonfire its safe...NVM you died again...
+var userInputName = prompt("Please enter your name.");
+if (userInputName != undefined ){
+  prompt("Welcome to Dark Souls " + userInputName + "!");
+} else {
+  prompt("Please enter your name!")
+}
