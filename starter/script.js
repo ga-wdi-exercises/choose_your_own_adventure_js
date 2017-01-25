4e22f682bbc0af4a7edfe311f24b4439bfef9947
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-01-24 23:02:30 -0500

    created alters and prompts up until pokemon selection

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,69 @@
 // put code here!
+//Pseudo-coded
+//Hello there! Welcome to the world of POKEMON! My name is OAK!
+alert("Hello there! Welcome to the world of Pokemon! My name is Oak!");
+//People call me the POKEMON PROF!
+alert("People call me the Pokemon Prof!");
+//This world is inhabited by creatures called POKEMON! For some people, POKEMON are pets. Others use them for fights. Myself...I study POKEMON as a profession.
+alert("This world is inhabited by creatures called Pokemon! For some people, Pokemon are pets. Others use them for fights. Myself...I study Pokemon as a profession.");
+//First, what is your name?
+//**Prompt: Input user's name.**
+//Returns: Right! So your name is userName!
+var userName = prompt("First, what is your name?");
+Response = alert("Right! So your name is "+userName+"!");
+//This is my grandson. He's been your rival since you were a baby.
+alert("This is my grandson. He's been your rival since you were a baby.");
+//...Erm, what is his name again?
+//**Prompt: input rival's name.**
+//Returns: That's right! I remember now! His name is rivalName!
+var rivalName = prompt("...Erm, what is his name again?");
+Response = alert("That's right! I remember now! His name is "+rivalName+"!");
+
+//userName! Your very own POKEMON legend is about to unfold! A world of dreams and adventures with POKEMON awaits! Let's go!
+alert(userName+"! "+"Your very own Pokemon legend is about to unfold! A world of dreams and adventures with Pokemon awaits! Let's go!");
+//Here, userName! There are 3 POKEMON here! Haha! They are inside the POKE BALLS. When I was young, I was a serious POKEMON trainer. In my old age, I have only 3 left, but you can have one! Choose!
+alert("Here, "+userName+"! There are 3 Pokemon here! Haha! They are inside the Poke Balls. When I was young, I was a serious Pokemon trainer. In my old age, I have only 3 left, but you can have one! Choose!");
+//Which POKEMON do you choose?
+//Option 1: Bulbasaur
+//Option 2: Squirtle
+//Option 3: Charmander
+//Option 4: Pikachu
+var pokemon = prompt("Which Pokemon do you chose? Bulbasaur (b), Squirtle (s), Charmander (c), or Pikachu (p).").toLowerCase();
+
+if (pokemon === "b"){
+  alert(userName+": Bulbasaur I choose you!");
+} else if (pokemon ==="s"){
+  alert(userName+": Squirtle I choose you!");
+} else if (pokemon ==="c"){
+  alert(userName+": Charmander I choose you!");
+} else if (pokemon ==="p"){
+  alert(userName+": Pikachu I choose you!");
+}
+//**user selects Bulbasaur**
+//b = prompt(rivalName+" choses Charmander and challenges you to a battle. Do you accept to battle. Do you accept to battle or do you run away?");
+//>Bulbasaur I choose you!
+//Q: rivalName choses Charmander and challenges you to a battle. Do you accept to battle or do you run?
+//>>>user accepts to battle.
+//>Q:Which finishing move do you select? Tackle or Growl?
+//>>>>Tackle, user wins.
+//>>>>Growl, user loses.
+//>>user runs from battle
+//>>user cannot escape, rival's POKEMON OHKO user. user loses.
+//**user selects Squirtle**
+//You've trained for months, won all the region's gym battles, and your Squirtle evolved into a Wartortle!
+//Wartortle wants to learn Hydro Pump but Wartortle already knows 4 moves! Should a move be forgotten to make space for Hydro Pump?
+//Yes. Which move should be forgotten?
+//Surf, Ice Beam, Rapid Spin, Aqua Ring?
+//1, 2 and...poof! Blastoise forgot [move] and learned Hydro Pump!
+//No. Blastoise did not learn Hydro Pump.
+//**user selects Charmander**
+//Charmander has fully evolved into a very powerful Charizard!
+//Remind me, what was Charizard's nickname?
+//Prompt input charizard's nickname
+//**user selects Pikachu**
+//You've been the elite four. What do you want to do next?
+//Go back to your home town to visit your mom, immidiately challenege the elite four to battle again, find and catch legendary pokemon?
+//Legendary pokemon, a wild Mew-Two appeared, what do you use to catch it?
+//Master ball or ultra balls.
+//Master ball successful.
+//Ultra ball, wild Mew-Two ran away.
