a8fe3a48c03caf3a078e0d3a389aa59782ca5d7c
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-25 18:08:22 -0500

    linked stylesheet and added opening alerts to users

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,4 @@
-// put code here!
+console.log('Hello World!');
+
+alert("Welcome to Dakota City! Home of our young hero and favorite Meta-Human, Virgil Hawkins/Static!");
+alert("Come join him on his adventures around Dakota City!");
