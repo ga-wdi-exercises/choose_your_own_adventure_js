35240ba3d2bcf048d421496a39941a7b22c174ee
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-25 18:27:27 -0500

    created prompt to enter adventure

--- starter/script.js
+++ starter/script.js
@@ -2,3 +2,8 @@ console.log('Hello World!');
 
 alert("Welcome to Dakota City! Home of our young hero and favorite Meta-Human, Virgil Hawkins/Static!");
 alert("Come join him on his adventures around Dakota City!");
+
+var userInput= prompt("Looks like Gotham City's infamous clown is on the loose in Dakota City creating a bit of trouble with the Meta-Breed Gang! Will you help Static battle and catch them? Enter 'yes' to continue!");
+    if (userInput == "yes") {
+      console.log("user entered adventure");
+}
