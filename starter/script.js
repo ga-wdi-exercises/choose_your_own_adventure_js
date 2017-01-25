11ff7dac9fda31129b4598a692c102a5e7c06f38
Collin OConnell <ococncol@gmail.com>
2017-01-24 21:02:54 -0500

    added a few prompts

--- starter/script.js
+++ starter/script.js
@@ -5,7 +5,7 @@
 //choose your class
 //Knight, Theif, Pyromancer
 
-//choose Knight
+//choose Knight   //choose Theif //choose Pyromancer
 //What weapon would you like? Sword and Sheild, Axe, Nothing(MAN-UP)
   //Choose Sword and Sheild
     //Choose your path
@@ -27,10 +27,21 @@
 
                   //Fall Down hole
                     //You Take Fall Damage, and the Boss Battle begins
+                      //Do you attack, roll to dodge, or try to run away..
+                        //You attack
+                          //You do a little damage but can't get out of the way fast enough
+                          // and You Die.
+                        //You Dodge
+                        // You roll to the right twice and end up behind the Boss
+                          //Do you wanna attack, roll, or run.
+                            //attack
+                              //You do enough damage to kill the boss and a Bonfire
+                              //takes its bodys place
+                                //You touch the Bonfire where it is safe... for a second
+                                //a dog comes out of nowhere and kills you.
 
-
-
-
+                        //Run away
+                          //You try to run away and get hit and then You Died.
             //Do Nothing
               //Spear comes out of the wall and You Died.
 
@@ -41,10 +52,9 @@
       //Pitfall onto Spikes. You Died.
 
 
-//Bonfire its safe...NVM you died again...
-var userInputName = prompt("Please enter your name.");
-if (userInputName != undefined ){
-  prompt("Welcome to Dark Souls " + userInputName + "!");
-} else {
-  prompt("Please enter your name!")
-}
+//code below
+var userInputName = prompt("Welcome to Dark Souls. What is your name");
+var response = prompt("Hello " + userInputName + "are you ready to play? (y/n)");
+  while (response != "y") {
+    response = prompt("Do you want to play?");
+  }
