8614317a64d96c3d20a796c0c664592c1ad3a9d6
Andy Flickinger <atflick@gmail.com>
2017-01-26 13:44:30 -0500

    cleaned up some code, fixed typos

--- starter/script.js
+++ starter/script.js
@@ -1,28 +1,19 @@
-// var userInput = prompt("Enter a string:");
-// while (userInput != "secret code") {
-//   userInput = prompt("Guess again to break the loop");
-// }
-//
-// function addition(x,y) {
-//   return x + y;
-// }
 
-// Pseudo code
 // Game of Life
-var startGame = prompt("Welcome to the game of life.  Continue through the game by entering the letter shown in brackets for each answer. Understoo[d]?");
+var startGame = prompt("Welcome to the Game of Life.  Continue through the game by entering the letter shown in brackets for each answer. Understoo[d]?").toLowerCase();
 while (startGame != "d") {
-  startGame = prompt("No, you must enter the letter in the brackets. Understoo[d]?");
+  startGame = prompt("No, you must enter the letter in the brackets. Understoo[d]?").toLowerCase();
 };
 // You were just born, are you a boy or girl?
-var genderType = ""
-var gender = prompt("Let's get started, you were just born, are you a [b]oy or [g]irl?");
+var genderType = "";
+var gender = prompt("Let's get started, you were just born, are you a [b]oy or [g]irl?").toLowerCase();
 while (gender != "b" && gender != "g") {
-  gender = prompt("Sorry, you must select either [b]oy or [g]irl to continue");
+  gender = prompt("Sorry, you must select either [b]oy or [g]irl to continue").toLowerCase();
 };
 
-if (gender === "b" || gender === "B") {
+if (gender === "b") {
   genderType = "boy";
-} else if (gender === "g" || gender === "G") {
+} else if (gender === "g") {
   genderType = "girl";
 };
 
@@ -95,9 +86,9 @@ function questionOne(){
     questionOne();
   }
 }
-
+// branch for working at mcDonalds
 function mcDonalds() {
-  var dietAnswer = prompt("McDonalds doesn't pay very well, but at least you get free food.  What do you like better, [B]ig Mac or [M]cChicken?");
+  var dietAnswer = prompt("McDonalds doesn't pay very well, but at least you get free food.  What do you like better, the [B]ig Mac or [M]cChicken?");
   if (dietAnswer == "b" || dietAnswer == "B") {
     alert("Your diet of Big Macs and years of drug abuse cause you to have a massive heart attack and die at age 30.");
   } else if (dietAnswer == "m" || dietAnswer == "M") {
@@ -107,9 +98,9 @@ function mcDonalds() {
     mcDonalds();
   }
 }
-
+// branch for working at radio shack
 function radioShack() {
-  var backToDrugs = prompt("Radio Shack has not been doing very well, sales across the country have plummeted, and your store closes. Tough times have come again, do you got back to using drugs? [Y]es or [N]o?")
+  var backToDrugs = prompt("Radio Shack has not been doing very well, sales across the country have plummeted, and your store closes. Tough times have come again, do you go back to using drugs? [Y]es or [N]o?")
   if (backToDrugs == "y" || backToDrugs == "Y") {
     var rehab = function() {
       var rehabAnswer = prompt("So you go back on drugs, but your family gets you to go on the show Intervention. They want to send you to rehab. Do you go? [Y]es or [N]o?");
@@ -128,7 +119,7 @@ function radioShack() {
   radioShack();
   }
 }
-
+// branch for working at barnes and noble
 function barnes() {
   var numOfKids = prompt("While stocking bookshelves, you catch the eye of a beautiful stranger. It was love at first sight and you end up getting married. How many kids do you end up having together? You can enter any number");
   if (numOfKids < 3) {
@@ -144,8 +135,7 @@ function barnes() {
     barnes();
   }
 }
-//
-
+// branch for art degree
 function art() {
   var artPath = prompt("Interesting choice, what do you plan to do with that after you graduate? [S]ell your art, [T]each art, or [O]ther")
   if (artPath == "s" || artPath == "S") {
@@ -159,7 +149,7 @@ function art() {
     art();
     }
 }
-
+// branch for CS degree
 function computerSci() {
   var csPath = prompt("Oh cool, so what do you think about this program? It [s]ucks or It's [c]ool");
   if (csPath == "s" || csPath == "S") {
@@ -172,6 +162,7 @@ function computerSci() {
     }
 }
 
+// branch for finance degree
 function finance() {
   var finPath = prompt("Sounds like you're into investing, did you invest in Tesla? [Y]es or [N]o");
   if (finPath == "y" || finPath == "Y") {
@@ -183,36 +174,3 @@ function finance() {
     finance();
   }
 }
-//
-
-
-//      -Oh no, that's bad! You get addicted to drugs and flunk out of school.  Years later you get clean, but your job options are limited.
-//        Where do you end up working? (McDonalds, Radio Shack, Barnes & Noble)
-//          McDonalds
-//            -McDonalds doesn't pay very well, but at least you get free food.  What do you like better? (Big Mac / McChicken)
-//              Big Mac - Your diet of Big Macs and years of drug abuse causes you to have a heart attack and die at age 30.
-//          Radio Shack
-//            -Radio Shack has not been doing very well and closes your store, do you got back to drugs? (Y / N)
-//              Y - You die at a very young age without much accomplished.
-//              N - Instead of buying drugs you buy a lottery ticket and end up hitting the jackpot.  You live the rest of your life comfortbaly, you lucky asshole.
-//          Barnes & Noble
-//            -While stocking books you meet your future spouse, how many kids do you have? (enter a number)
-//                - less than 3 - You and your family live comfortably ever after.
-//                - less than 6 - You and your family live on a tight budget, but you make it work and live a modest lifestyle.
-//                - greater than 5 - Whoa, you are unable to support that many kids with your current salary.  One of your kids becomes a stripper and your spouse leaves you because they think it's your fault.
-//    No
-//      -Drugs are bad, mmkay? You are good [boy/girl] and you get into college.  What do you major in? (Art, Computer Sciences, Finance)
-//        Art
-//          -Interesting choice, what are you going to do with that? (Become a teacher / Sell my art for a living / Other)
-//              T - Hey, the world needs teachers.  You live an altruistic life and are rewarded with good karma.
-//              S - You become the next Picasso and your name is taught in schools worldwide for decades into the future.
-//              O - Well I'm sure you will figure it out some day.
-//        Computer Sciences
-//          -Oh cool, so what do you think about this program? (It's good/ It sucks)
-//              G- You live happily ever after.
-//              S- You get hit by a bus and die.
-//        Finance
-//          -So you're into investing, how much money do you invest in Tesla?
-//              - < 10k - You should have invested more
-//
-//
