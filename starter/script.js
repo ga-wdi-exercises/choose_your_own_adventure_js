e0c8d80b083c5360bec9ac57990518c45c123d18
Collin OConnell <ococncol@gmail.com>
2017-01-26 18:50:51 -0500

    made function to run if statements and also added a switch

--- starter/script.js
+++ starter/script.js
@@ -1,54 +1,72 @@
 //Welcome to Dark Souls
 var response = prompt("Welcome to Dark Souls!\n Do you need help? \n (y/n)");
 // waiting for a yes response
-while (response === "n" || "N") {
+if(response === "n") {
   // if response is no
-    response = prompt("Did you Die Already?\n (y/n)");
-    switch (response) {
-      case 'y' || 'Y':
-        alert("You Died \n Refresh page to try again.");
-        break;
-      case 'n' || 'N':
-        alert("Well then lets get started!");
+  alert("Well then lets get started!!");
         //asking what class
-        // response = prompt("Is your class Knight, Theif or Pyromancer \n (k/t/p)");
-        // Knight
+      response = prompt("Is your class Knight, Theif or Pyromancer \n (k/t/p)");
+        //Knight
+          switch (response){
+            case 'k':
+            path();
+             break;
+           case 't':
+           path();
+          break;
+          case 'p':
+          path();
+           break;
+           default:
+           response = prompt("You Died thinking to hard!!");
+         }
+       }else{
+         alert('You do need help! \n You should walk away now..');
+       }
 
-          // if (response === 'k' || 'K'){
-            // Weapon
-            // response = prompt("Do you have a weapon? \n (y/n)");
-            // if (response === 'y' || 'Y'){
-              // Roll
-              // response = prompt("Do you know how to roll? \n (y/n)");
-              // if (response === 'n') {
-                // alert("You should learn too!");
-                // response = prompt("Are you ready to fight? \n (y/n)");
-                // if (response === 'y'){
-                  // alert("Walk into the mist \n and face the boss!");
-                  // response = prompt("Do you go straight and attack or roll left, or roll to the right? \n (s/l/r)");
-                  // if (response === 'l' || 'L'){
-                    // alert("You roll left, and dodge an attack.");
-                    // prompt("Do you want to attack now or roll again? \n (a/r)");
-                    // if(response === 'a' || 'A'){
-                      // alert("You attack, and do enough damaage to kill the Boss!");
-                      // prompt("Do you want to save at the bonefire? \n (y/n)");
-                      // if(response === 'y' || 'Y'){
-                        // alert("You survived!!!");
-                      // }
-                    // } else {
-                      // alert("You roll to the right, and get hit by attack.");
-                    // }
-                  // }
-
-                  // }
-                // }
-              // }
-        break;
-      default:
-        alert("You Died!");
-      }
-    }
-
-
-
-//here is where cut code goes
+//function
+function path(){
+  //Weapon
+  response = prompt("Do you have a weapon? \n (y/n)");
+  if (response === 'y'){
+    //Roll
+    response = prompt("Do you know how to roll? \n (y/n)");
+    // Learn
+    if (response === 'n') {
+      alert("You should learn too!");
+      response = prompt("Are you ready to fight? \n (y/n)");
+      // Fight
+      if (response === 'y'){
+        alert("Walk into the mist \n and face the boss!");
+        response = prompt("Do you go straight and attack or roll left, or roll to the right? \n (s/l/r)");
+        // Left, Straight, Right
+        if (response === 'l'){
+          alert("You roll left, and dodge an attack.");
+          //Move to the left
+          //Attack again? or Roll?
+            reponse = prompt("Do you want to attack now or roll again? \n (a/r)");
+           if (response === 'a'){
+             alert("You attack the boss and He dies!");
+             response = prompt("There is a bonfire... \n Do you want to save?\n (y/n)");
+           } else if (response === 'r'){
+             alert("You roll again and get hit by the Boss and Die");
+           } else {
+             alert("You don't move fast enough and get smacked across the room.");
+            }
+         } else if(response ==='s'){
+           alert("You try to attack and get hit and Die.");
+         } else if (response === 'r'){
+           alert("You roll right into an attack and you Die.");
+         } else {
+           alert("You get smashed by the boss");
+         }
+       } else {
+         alert("A skeleton comes up and kills you!");
+       }
+     } else {
+       alert("You keep rolling around, and end off falling off a cliff and then you Die.");
+     }
+   } else {
+     alert("You scrub...\n a cute little doggie come up and kills you because you\n think this game is easy.");
+   }
+}
