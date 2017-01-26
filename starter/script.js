fafdb3f9fb444bdb378aabce0f43ee5041bcde48
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-01-26 18:12:36 -0500

    completed path for Charmander

--- starter/script.js
+++ starter/script.js
@@ -5,7 +5,10 @@ alert("Hello there! Welcome to the world of Pokemon! My name is Oak!");
 //People call me the POKEMON PROF!
 alert("People call me the Pokemon Prof!");
 //This world is inhabited by creatures called POKEMON! For some people, POKEMON are pets. Others use them for fights. Myself...I study POKEMON as a profession.
-alert("This world is inhabited by creatures called Pokemon! For some people, Pokemon are pets. Others use them for fights. Myself...I study Pokemon as a profession.");
+var startGame = prompt("This world is inhabited by creatures called Pokemon! For some people, Pokemon are pets. Others use them for fights. Myself...I study Pokemon as a profession. Are you ready to being your adventure? (Yes)").toLowerCase();
+while (startGame != "yes"){
+  startGame = prompt("No, you must enter the letter in the parenthases. (Yes)");
+};
 //First, what is your name?
 //**Prompt: Input user's name.**
 //Returns: Right! So your name is userName!
@@ -22,13 +25,15 @@ Response = alert("That's right! I remember now! His name is "+rivalName+"!");
 //userName! Your very own POKEMON legend is about to unfold! A world of dreams and adventures with POKEMON awaits! Let's go!
 alert(userName+"! "+"Your very own Pokemon legend is about to unfold! A world of dreams and adventures with Pokemon awaits! Let's go!");
 //Here, userName! There are 3 POKEMON here! Haha! They are inside the POKE BALLS. When I was young, I was a serious POKEMON trainer. In my old age, I have only 3 left, but you can have one! Choose!
-alert("Here, "+userName+"! There are 3 Pokemon here! Haha! They are inside the Poke Balls. When I was young, I was a serious Pokemon trainer. In my old age, I have only 3 left, but you can have one! Choose!");
+alert("Here, "+userName+"! There are 3 Pokemon here! Haha! They are inside the Poke Balls. When I was young, I was a serious Pokemon trainer. In my old age, I have only 4 left, but you can have one! Choose!");
 //Which POKEMON do you choose?
 //Option 1: Bulbasaur
 //Option 2: Squirtle
 //Option 3: Charmander
 //Option 4: Pikachu
+
 var pokemon = prompt("Which Pokemon do you chose? Bulbasaur (b), Squirtle (s), Charmander (c), or Pikachu (p).").toLowerCase();
+
 //**user selects Bulbasaur**
 //>Bulbasaur I choose you!
 //Q: rivalName choses Charmander and challenges you to a battle. Do you accept to battle or do you run?
@@ -82,13 +87,32 @@ if (pokemon === "b"){
   }if (decision2 === "no"){
     alert("Wartortle did not learn Hydro Pump.");
   }
-
+//**user selects Charmander**
+//feed charmander rare candies until it evolves to charizard. Charmander>25>Charmilion>75>Charizard
 } else if (pokemon ==="c"){
   alert(userName+": Charmander I choose you!");
+  var decision3 = prompt("You're about to face the Elite Four, but your pokemon need some more training before challening them. You need to feed your Charmander some rare candies. How many rare candies do you want to feed your Charmander? Enter any number");
+  if (decision3 <= 5){
+    alert("Your Charmander is feeling much stronger!");
+  } else if (decision3 <= 20){
+    alert("What? Charmander is evolving!...Congratulations. Your Charmander evolved into a Charmeleon!");
+  } else if (decision3 <= 100){
+    alert("What? Charmander is evolving!...Congratulations. Your Charmander evolved into a Charmeleon!...What? Your Charmeleon is evolving!...Congratulations. Your Charmeleon evolved into a Charizard!");
+  } else {
+    alert("Your Charmander evolved straight into a Charizard and is beastly! Your team is ready to challenge the Elite Four.");
+}
+
+//**user selects Pikachu**
+//You've been the elite four. What do you want to do next?
+//Go back to your home town to visit your mom, immidiately challenege the elite four to battle again, find and catch legendary pokemon?
+//Legendary pokemon, a wild Mew-Two appeared, what do you use to catch it?
+//Master ball or ultra balls.
+//Master ball successful.
+//Ultra ball, wild Mew-Two ran away.
 } else if (pokemon ==="p"){
   alert(userName+": Pikachu I choose you!");
-  var decision4 = alert("You're Pikachu is a beast, chooses not to evolve into a Raichu, even after defeating the Elite Four. What do you do next?");
-  prompt("(A)Challenge the Elite Four again?, (B)Go back to your home town because you miss your mom?, (C)Find and Capture rare pokemon hidden around the region?").toLowerCase();
+  alert("You're Pikachu is a beast, chooses not to evolve into a Raichu, even after defeating the Elite Four. What do you do next?");
+  var decision4 = prompt("(A)Challenge the Elite Four again?, (B)Go back to your home town because you miss your mom?, (C)Find and Capture rare pokemon hidden around the region?").toLowerCase();
 
   switch (decision4){
     case "a":
@@ -102,10 +126,10 @@ if (pokemon === "b"){
     break;
 
     function challenge(){
-      alert("Stock up on Health Potions, Elixers, and Full Heals, because the Elite Four is even stronger now. But you and your Pikachu are up for the challenge.");
+      alert("Stock up on Health Potions, Elixers, and Full Heals, because the Elite Four is even stronger now. But you and your Pikachu are up for the challenge.");}
 
     function home(){
-      alert("You're home now and your mom cooked you and your pokemon an amazing homecoming meal. After you're finished eating, she keeps telling you to rest before you head out on yur adventure again.");
+      alert("You're home now and your mom cooked you and your pokemon an amazing homecoming meal. After you're finished eating, she keeps telling you to rest before you head out on yur adventure again.");}
 
     function rare(){
         var decision41 = prompt("After hours of exploring the Cerulean Cave near your home town, you encounter a phenomenal creature of myth. A wild Mew-Two appeared! You send out Pikachu to battle. What will you do? (A)Battle, (B)Bag: Use Master ball, (C)Switch Pokemon, (D)Run").toLowerCase();
@@ -117,28 +141,14 @@ if (pokemon === "b"){
         }else if(decision41 === "c"){
           alert("The rest of your pokemon have fainted. Pikachu is your only remaining pokemon to battle.");
           rare();
-        }else if(decison41 === "d"){
+        }else if(decision41 === "d"){
           alert("There's no escape.");
           rare();
         }
-      }
-    }
     }
 
   }
 } else {
   alert("Please select a valid Pokemon.")
+
 }
-
-
-
-
-//**user selects Charmander**
-//feed charmander rare candies until it evolves to charizard. Charmander>25>Charmilion>75>Charizard
-//**user selects Pikachu**
-//You've been the elite four. What do you want to do next?
-//Go back to your home town to visit your mom, immidiately challenege the elite four to battle again, find and catch legendary pokemon?
-//Legendary pokemon, a wild Mew-Two appeared, what do you use to catch it?
-//Master ball or ultra balls.
-//Master ball successful.
-//Ultra ball, wild Mew-Two ran away.
