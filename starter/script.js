fa53f579246f09d114b95733b3e905af65bd9459
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-26 08:51:33 -0500

    set up scene prompt

--- starter/script.js
+++ starter/script.js
@@ -10,3 +10,10 @@ var userInput= prompt("Looks like Gotham City's infamous clown is on the loose i
     if (userInput == "yes") {
       console.log("user entered adventure");
 }
+
+alert("While on patrol, Static picks up a distress call on a nearby police radio. 'Calling all units, Calling all units! Various reports of the Meta-Breed Gang wreaking havoc downtown with what appears to be a Clown. Units report!'");
+
+var userInput = prompt("Static is live on the scene of the mayhem and sees an unexpected Dark Knight! Will you help Static team up with Batman and bring the Meta-Breed Gang and the Joker to Justic? Enter 'yes' to continue!");
+    if (userInput == "yes") {
+      console.log("user is working with Batman");
+    }
