6ced9a55b05d60bb6dae04e352052188f9550311
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-26 13:20:15 -0500

    entered if else statement for first blow

--- starter/script.js
+++ starter/script.js
@@ -6,7 +6,7 @@ console.log('Hello World!');
 alert("Welcome to Dakota City! Home of our young hero and favorite Meta-Human, Virgil Hawkins/Static!");
 alert("Come join him on his adventures around Dakota City!");
 
-var userInput= prompt("Looks like Gotham City's infamous clown is on the loose in Dakota City creating a bit of trouble with the Meta-Breed Gang! Will you help Static battle and catch them? Enter 'yes' to continue!");
+var userInput = prompt("Looks like Gotham City's infamous clown is on the loose in Dakota City creating a bit of trouble with the Meta-Breed Gang! Will you help Static battle and catch them? Enter 'yes' to continue!");
     if (userInput == "yes") {
       console.log("user entered adventure");
 }
@@ -18,4 +18,13 @@ var userInput = prompt("Static is live on the scene of the mayhem and sees an un
       console.log("user is working with Batman");
     }
 
-alert("Joker, 'Hotstreak why don't you show Bats and Electric Boy what some real heat can do!");
+alert("Joker, 'Hotstreak why don't you show Bats and Electric Boy what some real heat can do!'");
+
+var userInput = prompt("Hotstreak charges at Static! Choose one of the following moves to help Static. Choose 'Ball Lightning to knock Hotstreek to the ground' or Choose 'Force Field to protect Static'");
+
+if (userInput == "Ball Lightning") {
+  console.log("Knocked Hotstreak off his feet");
+}
+  else {
+    console.log("Hotstreak blocked!");
+  }
