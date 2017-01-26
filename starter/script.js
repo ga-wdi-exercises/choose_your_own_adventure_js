addc945b244a8d1dff048135287893168560a738
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-26 04:47:39 -0500

    styled adventure page

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,8 @@
 console.log('Hello World!');
 
+// var image = new image();
+// image.src= '../static_shock.jpg';
+
 alert("Welcome to Dakota City! Home of our young hero and favorite Meta-Human, Virgil Hawkins/Static!");
 alert("Come join him on his adventures around Dakota City!");
 
