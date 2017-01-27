d9dbedd2e1e1808eb8b584fd3f9ed70a2f6795ff
Lisa Anne Burr <burr_lisa@bah.com>
2017-01-26 22:28:29 -0500

    started programming the if else logic

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("hello world");
+
+var start = prompt ("Hello! Shall we play a game? (Yes or No)");
+
+if (start = "Yes") {
+  alert("Awesome! You are in the shower soaping up when, suddenly, you hear your wife scream.  Startled, you drop the soap.  What do you do?");
+
+    var shower = prompt ("1 - Rinse off, dry off, get dressed then go downstairs. It's probably just a mouse.  OR 2 - Jump out of the shower, grab a towel and run downstairs to see what's going on.  I NEED TO RESCUE MY WIFE!");
+
+      if (shower = "1") {
+        alert("As you are rinsing off you get shampoo in your eyes.  You stumble around trying to get it rinsed out.  You step on the bar of soap, crash down and crack your skull open.  As your brains and blood ooze down the drain all you can think of is They are going to find me NAKED!  Everything goes dark.  The End.");
+      }
+       else {
+        var showerLive = prompt ("You get downstairs, soap drying into a sticky film on your skin you see your wife and son staring out the open front door.   You walk over and look out to see what they see.  There, on the street, are people running around getting...attacked?...by their…neighbors?  It looks like a riot has broken out on the streets of Little Turtle!  One of your neighbors, Bob, starts towards your house.  What do you do?");
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+else if (start = "No") {
+    alert ("Bummer…you're missing out on some serious action and adventure!");
+}
