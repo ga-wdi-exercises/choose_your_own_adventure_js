2f69433109d7237ed1ab2a692d0ddf8948c1c825
Collin OConnell <ococncol@gmail.com>
2017-01-26 20:07:05 -0500

    added a few more lines

--- starter/script.js
+++ starter/script.js
@@ -1,24 +1,24 @@
 //Welcome to Dark Souls
 var response = prompt("Welcome to Dark Souls!\n Do you need help? \n (y/n)");
 // waiting for a yes response
-if(response === 'n' || 'N' ) {
+if(response === 'n' ) {
   // if response is no
   alert("Well then lets get started!!");
         //asking what class
       response = prompt("Is your class Knight, Theif or Pyromancer \n (k/t/p)");
           switch (response){
             //KNight
-            case 'k' || 'K':
+            case 'k':
             alert('You choose the Knight class!');
             path();
              break;
             //Theif
-           case 't' || 'T':
+           case 't':
            alert('You choose the Theif class!');
            path();
           break;
           //Pyromancer
-          case 'p' || 'P':
+          case 'p':
           alert('You choose the Pyromancer class!');
           path();
            break;
@@ -33,19 +33,20 @@ if(response === 'n' || 'N' ) {
 function path(){
   //Weapon
   response = prompt("Do you have a weapon? \n (y/n)");
-  if (response === 'y' && 'Y'){
+  if (response === 'y'){
     //Roll
     response = prompt("Do you know how to roll? \n (y/n)");
     // Learn
-    if (response === 'n' && 'N') {
+    if (response === 'n') {
       alert("You should learn too!");
+      alert("If you don't know how to use Google!");
       response = prompt("Are you ready to fight? \n (y/n)");
       // Fight
-      if (response === 'y' && 'Y'){
+      if (response === 'y'){
         alert("Walk into the mist \n and face the boss!");
         response = prompt("Do you go straight and attack or roll left, or roll to the right? \n (s/l/r)");
         // Left, Straight, Right
-        if (response === 'l' && 'L'){
+        if (response === 'l'){
           alert("You roll left, and dodge an attack.");
           //Move to the left
           //Attack again? or Roll?
@@ -63,9 +64,9 @@ function path(){
            } else {
              alert("You don't move fast enough and get smacked across the room.");
             }
-         } else if(response ==='s' && 'S'){
+         } else if(response ==='s'){
            alert("You try to attack and get hit and Die.");
-         } else if (response === 'r' && 'R'){
+         } else if (response === 'r'){
            alert("You roll right into an attack and you Die.");
          } else {
            alert("You get smashed by the boss");
