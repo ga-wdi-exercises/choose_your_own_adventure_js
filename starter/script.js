ed1d2ec18cf7dfc439798a9d5a23c9a265613f1f
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-01-26 23:32:41 -0500

    javascriptpractice

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,60 @@
-// put code here!
+var basketball = prompt ("Do you like basketball?")
+//Answer = Yes//
+
+
+if ("yes") {
+  basketball.good ("ask favorite team");
+
+} else {
+  basketball.good ("ask about tennis");
+  }
+
+
+var team = prompt ("Who's your fav team? Warriors, Cavs, Knicks, or Bulls")
+//Answer = Warriors//
+
+
+var stefKlay = prompt("Who do you prefer, Stef or Klay?")
+//Stef//
+//Good choice, Klay tends to dissapear in the clutch.//
+
+
+var cluth = prompt ("What about Kevin Durant or Draymond Green")
+//Kevin Durant//
+//Good call. Draymond Green is a cry baby.//
+
+var cavs = prompt ("Are you a Cavs fan?")
+//yes//
+
+
+var kyrieLebron = prompt (Do you prefer Lebron or Kyrie)
+//Lebron//
+
+if ("Lebron > Kyrie") {
+    Lebron.kyrie ("What about the Knicks")
+}   else {
+    Lebron.kyrie("Inform Ted he's crazy");
+  }
+
+Var knicks = prompt ("What about the Knicks? Do you like Carmello, Rose, or Noah")
+//Carmello//
+//Bad choice. Carmello sucks//
+
+Var rose = prompt ("What about Derrick Rose"?)
+//D guy suck. Nobody like d guy.//
+
+Var bulls = ("Who's your favorite player on the Bulls. Wade, Rondo, or Rose.")
+//Wade//
+//Good idea. D Wade's got some heart.//
+
+Var butler = ("What about Jimmy Butler? Do you think he's good?")
+//Butlers good//
+
+Var offense = ("Do you think he's better on offense or defense")
+//Offense//
+
+if (offense > defense) {
+    butler.bulls ("End Convo");
+  } else {
+    butler.bulls ("continue thread")
+  }
