a80c2b63759ab69f23d5c1748d06cd2a37647206
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-01-27 08:52:40 -0500

    choose your own adv.

---
+++ main.js
@@ -0,0 +1,32 @@
+//("Hello, what is your name?")
+var name= prompt("What is your name?");
+//console.log("Nice to meet you 'name'
+//("Nice to meet you")
+//("What is your favorite meal of the day?")
+var favMeal=("What is your favorite meal of the day?")
+  //if Breakfast
+//Breakfast
+//Do you have time morning to fix something or are you the type of person that likes to grab something on the go?")
+var breakfast= prompt("Do you have time morning to fix something or are you the type of person that likes to grab something on the go?")
+//Time to cook
+  var timeToCook= alert("Breakfast literally means to break the fast after a long night's sleep. Eating breakfast has been shown to improve mental alertness and physical performance.")
+  //Breakfast literally means to break the fast after a long night's sleep. Eating breakfast has been shown to improve mental alertness and physical performance.
+  var noTime= alert("I get it, sometimes hitting the snooze button is worth it.")
+  //I get it, sometimes hitting the snooze button is worth it.
+
+//Lunch
+var lunch= prompt("Do you generally bring your lunch or do you usually go out to lunch?")
+  //Do you generally bring your lunch or do you usually go out to lunch?
+  var bringLunch=alert("The concept of bringing a meal to work dates back to the mid-1800s. The tradition started because farmers and industrial workers, who couldn't come home at noon, bought food such as hard-boiled eggs, veggies, and meat into old tobacoo tins.")
+    //bring lunch
+      //The concept of bringing a meal to work dates back to the mid-1800s. The tradition started because farmers and industrial workers, who couldn't come home at noon, bought food such as hard-boiled eggs, veggies, and meat into old tobacoo tins.
+  var outToEat=alert("Check out this DC lunch guide. https://www.thrillist.com/eat/washington-dc/the-17-best-washington-dc-neighborhood-lunch-spots")
+    //go out to eat
+      //
+var dinner= prompt("Do you enjoy cooking, prefer take out, or dinner with friends?")
+  //Dinner
+    //Do you enjoy cooking, prefer take out, or dinner with friends?
+
+      var cooking=alert("I love cooking too!")
+      var takeOut=alert("Interested in some new suggestions? Here is a list of affordable options in DC offering carryout https://www.washingtonian.com/2016/05/18/best-cheap-carryout-restaurants-dc-maryland-virginia/")
+      var dinWF=alert("Great choice. Here is a list of highly recommended restaurants in the area http://dc.eater.com/maps/best-dc-restaurants-38")
