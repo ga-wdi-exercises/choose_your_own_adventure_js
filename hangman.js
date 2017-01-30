0cf231ef4e8b009804f2b4237cde8a15dd6d44d1
xgironx <xgironx@gmail.com>
2017-01-30 15:22:50 -0500

    prior to changes identify all instances of a match

--- hangman.js
+++ hangman.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   console.log("Hangman Game");
 });
 
-var btnEnterYerWord = $("#btnEnterYerWord");
-var textInputPlayerOnesWord = $("#textInputPlayerOnesWord");
+var btnEnterYerWord =           $("#btnEnterYerWord");
+var textInputPlayerOnesWord =   $("#textInputPlayerOnesWord");
 var playerOnesWordInput;
 var playerOnesWordSplitArray;
-var btnPlayerTwosGuess = $("#btnPlayerTwosGuess");
-var textInputPlayerTwosGuess = $("#textInputPlayerTwosGuess");
+var btnPlayerTwosGuess =        $("#btnPlayerTwosGuess");
+var textInputPlayerTwosGuess =  $("#textInputPlayerTwosGuess");
 
 btnEnterYerWord.on('click', function (event) {
   event.preventDefault();
@@ -15,15 +15,17 @@ btnEnterYerWord.on('click', function (event) {
                               console.log(textInputPlayerOnesWord.val());
   playerOnesWordInput = textInputPlayerOnesWord.val();
                               console.log("heres playerOnesWord:" + playerOnesWordInput);
-  playerOnesWordSplitArray = playerOnesWordInput.split(/(?!$)/);     //this works: //"overpopulation".split(/(?!$)/)
+  // playerOnesWordSplitArray = playerOnesWordInput.split(/(?!$)/);     //this works: //"overpopulation".split(/(?!$)/)
+  playerOnesWordSplitArray = playerOnesWordInput.split('');     //this works: //"overpopulation".split(/(?!$)/)
                               console.log("heres playerOnesWordSplitArray:" + playerOnesWordSplitArray);
   fillOutVannaWhitesWall();
 });
+
 function fillOutVannaWhitesWall() {                       //console.log('filling in Vanna');
   playerOnesWordCssClass = $("#playerOnesWord");
   for (i=0; i < playerOnesWordSplitArray.length; i++){
     var thisLetter = playerOnesWordSplitArray[i];         //console.log('thisLetter:' + thisLetter);
-    var div = $('<div id="div'+ i +'"  class="aLetter"> '+ thisLetter +' </div>');    //console.log('were on div: ' + i);
+    var div = $('<div id="div   ' + i + ' class="aLetter" >  ' + thisLetter + '  </div>');    //console.log('were on div: ' + i);
     playerOnesWordCssClass.append(div);
   }
 }
@@ -33,22 +35,23 @@ function fillOutVannaWhitesWall() {                       //console.log('filling
 
 btnPlayerTwosGuess.on('click', function (event) {
   event.preventDefault();
-                                                  console.log(textInputPlayerTwosGuess.val());
+                              console.log(textInputPlayerTwosGuess.val());
   playerTwosGuess = textInputPlayerTwosGuess.val();
   comparo();
 });
 function comparo() {                                //console.log('filling in Vanna');
-  playerTwosGuessCssClass = $("#playerTwosGuess");
+  //^^ dont use /////// maybe... addClass('playerTwosGuess')//////////////
   for (i=0; i < playerOnesWordSplitArray.length; i++){
-    var thisGuess = playerTwosGuess;            //console.log('thisGuess:' + thisGuess);
-    console.log('thisWord: ' + playerOnesWordInput);
-    console.log('thisGuess:' + thisGuess);
-    if ($.inArray(thisGuess, playerOnesWordSplitArray) == true){
-      console.log('thisGuess:' + thisGuess + ' is in thisWord: ' + playerOnesWordInput);
+    console.log('playerTwosGuess: ' + playerTwosGuess);
+                              console.log( 'thisWord: ' + playerOnesWordInput);
+                              console.log( 'thisGuess:' + playerTwosGuess);
+
+    if ($.inArray(playerTwosGuess, playerOnesWordSplitArray) !== -1){
+                              console.log( 'thisGuess:   ' + playerTwosGuess + '   is in thisWord:  ' + playerOnesWordInput);
       $('#playerTwosGuess').append('+');    //console.log('were on div: ' + i);
     }
     else {
-      console.log('suckah');
+                              console.log( 'suckah' );
     }
 
   }
