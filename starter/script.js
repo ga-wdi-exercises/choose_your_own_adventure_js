e287bee5705215868cbf81f872e1ca2effae80ed
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-03-25 13:54:07 -0400

    Martin Show Game added Martin's choice

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,24 @@
 // put code here!
+var answer = prompt("Let's Play Martin the TV Show! Which character are you?");
+
+if (answer == "Martin") {
+prompt("You the Man! Would you like to deal with Brotha Man or Ms. Geri?")
+
+    if ("Brotha Man") {
+        prompt("Brotha Man wants a sandwich? Do you give him a sandwich or kick him out?");
+        if ("sandwich") {
+          alert ("BAD IDEA! You'll need more than a snadwich to feed him?")
+        } else if("kick him out") {
+          alert ("GOOD IDEA! Brotha Man will eat you out of house and home.")
+        }
+    }
+
+    if ("Ms. Geri") {
+      prompt("Ms. Geri is ready to fight because you cut the line at the DMV. Do you fight or flight?")
+       if ("fight") {
+        prompt("BAD IDEA! Ms. Geri is a bar knockles boxing champ.")
+      } else if ("flight") {
+        prompt("GOOD IDEA! Ms. Geri would've knocked you out with one punch.")
+      }
+    }
+}
