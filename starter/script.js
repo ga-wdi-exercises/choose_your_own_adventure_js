747b9aaa57b05b6d542d125e60f838644677e925
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-03-25 14:24:24 -0400

    added 2nd choice to Martin but error: have to choose it twice for game to work.

--- starter/script.js
+++ starter/script.js
@@ -1,24 +1,34 @@
 // put code here!
-var answer = prompt("Let's Play Martin the TV Show! Which character are you?");
+var user = prompt("Let's Play Martin the TV Show! Which character are you?");
 
-if (answer == "Martin") {
+if (user == "Martin") {
 prompt("You the Man! Would you like to deal with Brotha Man or Ms. Geri?")
 
-    if ("Brotha Man") {
-        prompt("Brotha Man wants a sandwich? Do you give him a sandwich or kick him out?");
-        if ("sandwich") {
-          alert ("BAD IDEA! You'll need more than a snadwich to feed him?")
-        } else if("kick him out") {
-          alert ("GOOD IDEA! Brotha Man will eat you out of house and home.")
-        }
-    }
-
-    if ("Ms. Geri") {
-      prompt("Ms. Geri is ready to fight because you cut the line at the DMV. Do you fight or flight?")
-       if ("fight") {
-        prompt("BAD IDEA! Ms. Geri is a bar knockles boxing champ.")
-      } else if ("flight") {
-        prompt("GOOD IDEA! Ms. Geri would've knocked you out with one punch.")
-      }
-    }
+ if ("Brotha Man"){
+            var bm = prompt("Brotha Man wants a sandwich? Do you give him a sandwich or kick him out?");
+              switch (bm) {
+                case "sandwich":
+                  alert ("BAD IDEA! You'll need more than a sandwich to feed him?");
+                case "kick him out":
+                  alert("GOOD IDEA! Brotha Man will eat you out of house and home.");
+                 break;
+                 default:
+                   console.log("You must choose sandwich or kick him out.")
+                  break;
+                } else if ("Ms Geri") {
+            var mg = prompt("Ms. Geri is ready to fight because you cut the line at the DMV. Do you fight or flight?");
+              switch (mg) {
+                case "fight":
+                  alert ("BAD IDEA! Ms. Geri is a bar knuckles boxing champ.");
+                case "flight":
+                  alert("GOOD IDEA! Ms. Geri would've knocked you out with one punch.");
+                break;
+                default:
+                  console.log("You must choose sandwich or kick him out.")
+                break;
+}
+} else {
+  console.log("You must choose Brotha Man or Ms. Geri.")
+}
+}
 }
