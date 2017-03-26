59f7d218c1180714e2af9c73c42b2e0d1647a10d
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-25 21:40:07 -0400

    complete left option conditional

--- starter/script.js
+++ starter/script.js
@@ -12,33 +12,33 @@ var name = prompt("Welcome to High School! What's your name?").toLowerCase();
 var direction = prompt("Hi" + " " + name + "! Here's a welcoming gift *hands you a 10 pack of gum* Try not finish it all in one day! Time for a long day of classes. Which direction will you go? Straight, Left, Right, or Exit?").toLowerCase();
 
 switch(direction) {
+  // Straight
     case 'straight':
-        var teacher = prompt("You run into a teacher who thinks you're tardy and is very upset. Do you 1: offer them some gum or 2: explain your way out?").toString();
-        if (teacher === '1') {
-          alert("Is that a bribe? That's detention and I'm confiscating that gum! GAME OVER")
-        } else {
-          alert("My mistake, I have a hard time distinguishing the bell from the ringing in my ears. Have a great day!")
+    // You run into a teacher who thinks you're tardy and is very upset. Do you 1: offer them some gum or 2: explain your way out?
+        var straight = prompt("You run into a teacher who thinks you're tardy and is very upset. Do you 1: offer them some gum or 2: explain your way out?").toString();
+        // User input 1: Offer a piece of gum so they get off your case
+        if (straight === '1') {
+          alert("Is that a bribe? That's detention and I'm confiscating that gum! GAME OVER!");
         }
+        // User input 2: Explain your way out of the situation
+        else {
+          alert("My mistake, I have a hard time distinguishing the bell from the ringing in my ears. Have a great day! YOU WIN!");
+        };
         break;
+  // Left
+    case 'left':
+        var left = prompt("You take the long way to class. You're feeling a little peckish from the walk so you eat a piece of gum. Someone sees it and asks for a piece. Do you 1: oblige or 2: refuse?").toString();
+        // User input 1: Sure, I still have 9 pieces left!
+        if (left === '1') {
+          alert("'Aww thanks!' you catch a whiff of their horrifying breath and your noble act lifts your mood for the entire day. YOU WIN!");
+        }
+        // User input 2: Don't give them a piece. You only have 9 left!
+        else {
+          alert("'You're a jerk' the someone breathes hot stinky breath in your face, you pass out from the toxins and they steal your entire pack. GAME OVER!")
+        }
     default:
         prompt("No info")
 }
-
-// Straight
-  // You run into a teacher who thinks you're tardy and is very upset. Do you 1: offer them some gum or 2: explain your way out?
-    // 1: Offer a piece of gum so they get off your case
-      // "Is that a bribe? That's detention and I'm confiscating that gum!"
-        // Game over!!
-    // 2: Explain your way out of the situation
-      // "My mistake, I have a hard time distinguishing the bell from the ringing in my ears. Have a great day!"
-
-// Left
-  // You take the long way to class. You're feeling a little peckish from the walk so you eat a piece of gum. Someone sees it and asks for a piece. Do you 1: oblige or 2: refuse?"
-    // 1: Sure, I still have 9 pieces left!
-      // "Aww thanks!" you catch a whiff of their horrifying breath and your noble act lifts your mood for the entire day.
-    // 2: Don't give them a piece. You only have 9 left!
-      // "You're a jerk" the someone breathes hot stinky breath in your face, you pass out from the toxins and they steal your entire pack.
-        // Game over!!
 
 // Right
   // Your crush is walking towards you. They're wearing a t-shirt that says I HEART GUM. Do you want to 1: Offer them your entire pack or 2: Offer them a piece?
