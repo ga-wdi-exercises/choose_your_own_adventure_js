3c665e6971fef7c4a51d3e476bb0ab21b4abf0f7
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-03-27 14:08:36 -0400

    else if and switch statement working

--- starter/script.js
+++ starter/script.js
@@ -1,30 +1,35 @@
 // put code here!
-var user = prompt("Let's Play Martin the TV Show! You the Man! Would you like to deal with Brotha Man or Ms. Geri?");
-while (user == "Brotha Man" || user =="Ms. Geri") {
-}
+var user = prompt("Let's Play Martin the TV Show! Type Martin to start the game" );
 
-var optionOne = "Brotha Man"
-var optionTwo = "Ms. Geri"
+  if (user == "Martin") {
+  var deal = prompt("You the Man! Would you like to deal with Brotha Man or Ms. Geri?");
 
-  switch (user === "Martin") {
-    case 1: optionOne
+
+
+    if (deal == "Brotha Man"){
               var bm = prompt("Brotha Man wants a sandwich? Do you give him a sandwich or kick him out?");
-                if (bm === "sandwich") {
+                switch (bm) {
+                  case "sandwich":
                     alert ("BAD IDEA! You'll need more than a sandwich to feed him?");
-                  } else if (bm =="kick him out") {
+                  case "kick him out":
                     alert("GOOD IDEA! Brotha Man will eat you out of house and home.");
-                  }
-                  break;
-
-
-     case 2:  optionTwo
-                var mg = prompt("Ms. Geri is ready to fight because you cut the line at the DMV. Do you fight or flight?");
-                  if (mg === "fight") {
-                    alert ("BAD IDEA! Ms. Geri is a bar knuckles boxing champ.");
-                  } else if (mg === "flight") {
-                    alert("GOOD IDEA! Ms. Geri would've knocked you out with one punch.");
-                  }
+                   break;
+                   default:
+                     console.log("You must choose sandwich or kick him out.")
                     break;
-                    default:
-                    console.log();
-              }
+                  }
+  } else if (deal == "Ms. Geri") {
+              var mg = prompt("Ms. Geri is ready to fight because you cut the line at the DMV. Do you fight or flight?");
+                switch (mg) {
+                  case "fight":
+                    alert ("BAD IDEA! Ms. Geri is a bar knuckles boxing champ.");
+                  case "flight":
+                    alert("GOOD IDEA! Ms. Geri would've knocked you out with one punch.");
+                  break;
+                  default:
+                    console.log("You must choose sandwich or kick him out.")
+                  break; }
+  } else {
+    console.log("You must choose Brotha Man or Ms. Geri.")
+  }
+  }
