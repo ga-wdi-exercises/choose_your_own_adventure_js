6316ca133aee754d6f65bfebf77087f61ac924d1
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-03-27 23:13:18 -0400

    added more characters

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,8 @@
 // put code here!
-var user = prompt("Let's Play Martin the TV Show! Type Martin to start the game" );
+var user = prompt("LET's PLAY MARTIN TV SHOW! Type Martin to start the game" );
 
   if (user == "Martin") {
-  var deal = prompt("You the Man! Would you like to deal with Brotha Man or Ms. Geri?");
+  var deal = prompt("YOU THE MAN! Would you like to deal with Brotha Man, Ms. Geri, Pam, Mad Dog No Good, Ricky Fontaine or Varnell Hill? ");
 
 
 
@@ -18,6 +18,7 @@ var user = prompt("Let's Play Martin the TV Show! Type Martin to start the game"
                      console.log("You must choose sandwich or kick him out.")
                     break;
                   }
+
   } else if (deal == "Ms. Geri") {
               var mg = prompt("Ms. Geri is ready to fight because you cut the line at the DMV. Do you fight or flight?");
                 switch (mg) {
@@ -27,7 +28,41 @@ var user = prompt("Let's Play Martin the TV Show! Type Martin to start the game"
                     alert("GOOD IDEA! Ms. Geri would've knocked you out with one punch.");
                   break;
                   default:
-                    console.log("You must choose sandwich or kick him out.")
+                    console.log("You must choose fight or flight.")
+                  break; }
+
+  } else if (deal == "Pam") {
+              var mg = prompt("Pam just came from the beauty salon and her weave is on point. Do you give her a compliment or inslut?");
+                switch (mg) {
+                  case "insult":
+                    alert ("BAD IDEA! Pam is ready to snap back with an insult she said: 'Not today Sir Short A Lot, now move before I crush you with my pinky toe!'");
+                  case "compliment":
+                    alert("GOOD IDEA! Pam says: 'I know I look Short Doggy Dog, now get out of my way!'");
+                  break;
+                  default:
+                    console.log("You must choose compliment or insult.")
+                  break; }
+  } else if (deal == "Mad Dog No Good") {
+              var mg = prompt("Mad Dog No Good, just cam home from prison. He went to prison because you testified against him. Do you stay or do you go?");
+                switch (mg) {
+                  case "stay":
+                    alert ("BAD IDEA! Pam is ready to snap back with an insult she said: 'Not today Sir Short A Lot, now move before I crush you with my pinky toe!'");
+                  case "go":
+                    alert("GOOD IDEA! Pam says: 'I know I look Short Doggy Dog, now get out of my way!'");
+                  break;
+                  default:
+                    console.log("You must choose stay or go.")
+                  break; }
+  } else if (deal == "Ricky Fontaine") {
+              var mg = prompt("Ricky Fontaine, is at your high school reunion and he's making fun or you. Do you kick butt...?");
+                switch (mg) {
+                  case "kick butt":
+                    alert ("BAD IDEA!");
+                  case "kiss my butt":
+                    alert("GOOD IDEA! !'");
+                  break;
+                  default:
+                    console.log("You must choose sandwich or kick butt or kiss my butt.")
                   break; }
   } else {
     console.log("You must choose Brotha Man or Ms. Geri.")
