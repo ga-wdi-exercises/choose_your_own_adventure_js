39a8bb44ee92324ab9ac8d5eb2926ef046dcf5ee
Matt Caplan <mattpcaplan@gmail.com>
2017-03-28 10:59:55 -0400

    first commit

---
+++ CYOA/script.js
@@ -0,0 +1,35 @@
+alert("Welcome to the (Im)morality game!  The game that takes you on a road to the low point of your life!")
+confirm("Readyyyy to rumble????");
+
+var swag = prompt("On a scale of 1-10; how much swag ya got?")
+if(swag <= 6)
+  {
+    alert("Commmmmon, you're swaggier than dat!  Bring your A game!")
+  }
+else
+  {
+    alert("Yeahhhhhhh you cocky S.O.B!  Let's play!")
+  }
+
+
+var choice1 = prompt("You are at a house party, you see an unattended police helicopter on an adjacent rooftop.  The keys are in it!  Go for a spin?")
+
+if(choice1 == "Yes")
+{
+  alert("Wow - I've never drank 151 this high up in the sky!...Why are all these other choppers chasing me??....")
+}
+  else {
+    alert("Yeah those crazy days are behind me.  Time to watch a Friends marathon.....SCREW IT -- IT'S GO TIME")
+  }
+
+  var choice2 = prompt("Should we fly to the Mall or to Ihop?")
+    if(choice2 == "Mall") {
+      alert("Yay!  Burberry and tears!...Where did all these cops come from?....")
+    }
+    if(choice2 == "Ihop") {
+      alert("Mmmmmmmmmmm maple, marshmellow, blueberry, apricot pancakes!  Oh daaaaamn, where did all these cops come from?.....")
+    }
+    else {
+      alert("Ahhhhhhhh we're going down! I really hope this doesn't make the local news...")
+    }
+alert("I hope you learned something from this exercise.  Police helicopters are the property of the state and NOT your drunken whims.")
