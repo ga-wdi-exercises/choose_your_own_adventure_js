ea11a7479d6f3678690537884ab57282f82a7daf
Luisa Gonzalez <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-03-30 11:41:43 -0400

    Neverending Adventure

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,94 @@
 // put code here!
+var name=prompt('What is your name?')
+ // ask which quest to discover
+ var quest=prompt('What quest would you like to go on? Save the Empress or Find the treasure? (e/t) ')
+
+ if(prompt=="e"){
+  //  Directions to the mission
+  var direction=prompt('what direction would you like to go? (r/l)')
+  if(direction=="r"){
+    alert("right takes you through the dark forest")}
+  else if(direction=="l"){
+    alert("left takes you through the mountains")
+    }
+if(prompt=="Dark forest route"){
+   alert("challenge the forest monster Gmork")
+} else if(prompt=="Mountain pass route"){
+   alert("challenge the Rock Biter")
+} else ("Sudden Death=Sphinx Gate")
+
+}
+
+var weapon=prompt('what weapon would you like to use to fight Gmork?(S/BA/AP)')
+if(weapon=="S"){
+alert("You've chosen a sword to kill the Gmork")}
+if(weapon=="BA"){
+alert("You've chosen a Bow and Arrow to kill Gmork")}
+if(weapon=="AP"){
+alert("You've chosen to use the Auryn's Power to Defeat Gmork")
+}
+
+var weapon=prompt('what weapon would you like to use to fight Rock Biter?(WH/AP)')
+if(weapon=="WH"){
+alert("You've chosen a warhammer to fight Rock Biter")
+if(weapon=='AP'){
+alert("You've chosen the Auryn's power to defeat Rock Biter")
+}
+
+var weapon=prompt('what weapon would you like to use to challenge the Sphinx?(AP)')
+if(weapon=="AP"){
+alert("You've chosen to face the Sphinx using only the Auryn's power")
+}
+var Luckdragon=prompt("You've defeated Gmork, would you like to continue to find the Luckdragon? (Y/N)")
+if(prompt=="Y"){
+alert("Auryn will give you the power to call Falkor, the Luckdragon")}
+if(prompt=="N"){
+alert("You have no hope in saving the empress, therefore, you cannot continue on with your mission")
+}
+
+var Rockbiter=prompt("You've defeated the Rockbiter, would you like to continue to find the Luckdragon? (Y/N)")
+if(prompt=="Y"){
+alert("Auryn will give you the power to call Falkor, the Luckdragon")}
+if(prompt=="N"){
+alert("You have no hope in saving the empress, therefore, you cannot continue on with your mission")}
+}
+
+var Southern_Oracle=prompt("You've reached the Southern Oracle, would you like to complete the mission? (Y/N)")
+if(prompt=="Y"){
+alert("The Auryn to guide you to the Ivory Tower where the Empress is imprisoned")}
+if(prompt=="N"){
+alert("You have no hope in saving the empress, therefore, you cannot continue on with the mission")}
+
+
+var Ivory_Tower=prompt("Congratulations, you've reached the Ivory Tower, home of the Empress. Are you ready to fight the evil wizard who has imprisoned the empress? (Y/N)")
+if(prompt=="Y"){
+alert("You will have to fight the wizard only using the power of the auryn")}
+if(prompt=="N"){
+alert("You have no hope in saving the empress, therefore, you cannot continue on with your mission")}
+
+var empress=prompt("Congratulations, you've defeated the wizard using the power of the Auryn. Would you like to rescue the empress? (Y/N)")
+if(prompt=="Y"){
+alert("The auryn will transform into a key. You can go to the the empresse's quarters to unlock her chamber and rescue her")}
+if(prompt=="N"){
+alert("You have no hope in saving the empress, therefore, you cannot continue on with your mission")}
+
+var mission_complete=prompt("Congratulations, you've rescued the empress and saved Fantasia. Do you accept the royal gift from the Empress? (Y/N)")
+if(prompt=="Y"){
+alert("You've saved Fantasia and the Empress. You are awarded a medal of courage and honor")}
+if(prompt=="N"){
+alert("You have not accepted the royal gift, but you have completed your mission, and the empress is grateful for it")}
+
+
+
+
+
+   // left
+   // look for pass through the mountains
+   // right
+
+ // save the childlike empress
+  // Directions to the mission
+  // left
+  // look for pass through the mountains
+  // right
+ // find the hidden treasure in Fantasia
