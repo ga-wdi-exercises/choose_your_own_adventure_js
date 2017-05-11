f971b68fb90b2d96ae9ecfd07bd3ea8e17e92b23
ANTWANA WADLINGTON <antwana.wadlington@gmail.com>
2017-05-11 09:11:31 -0400

    AntwanaW HW

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,76 @@
-// put code here!
+//Beauty and the Beast Choose Your Own adventure
+var userResponse;
+//Belle leaves home to go to town
+var belle;
+console.log("Belle goes to town")
+//Choose to visit the baker, bookstore policeStation or grandmasHouse
+var bookstore;
+var policeStation;
+var grandmasHouse;
+
+switch(journey)
+{
+  case "bookstore":
+    confirm("Go to the bookstore?");
+    break;
+  case "Police station":
+    confirm("Go to the Police Station?");
+    break;
+  case "Grandma's House";
+    confirm("Go to Grandma's House?");
+}
+if userResponse === bookstore{
+  confirm("have some tea and read a book"){
+    
+  }
+  confirm("borrow a book and go home")
+  confirm("return a book and go to the spa")
+}
+if userResponse === policeStation{
+  confirm("get a restraining order against Gaston")
+  confirm("report father as missing")
+  confirm("")
+}
+if userResponse === grandmasHouse{
+  confirm("Have a girls night out with grandma")
+  confirm("Help make dinner")
+  confirm("Go to the casino")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var displayOptions['baker', 'bookstore', 'policeStation', 'grandmasHouse']{
+  console.log()
+  if userResponse === baker
+
+
+}
+
+//She visits either the baker, bookstore, or the poice station
+var baker;
+
+var bookstore
+var policeStation
+var grandmasHouse
+//bookstore
+
+//baker
+
+//Police station
+
+//Grandma's House
