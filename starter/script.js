40af2dfe5b3c9bb514c074d44103be7620d51f41
Scott Pierce <scottpiercew@gmail.com>
2017-05-11 06:11:33 -0400

    four outcomes and while loop

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,37 @@
-// put code here!
+var person = prompt("Please enter your name:");
+if (person != null) {
+// $(".response").append(" Hello " + person + "! Ready for today's adventure?");
+
+
+var lv1 = prompt("Hello " + person + "!\n" + "I am C-3PO, human cyborb relations. Is Darth Vader your father?\n" + "yes/no")
+if (lv1 == "yes") {
+
+  var lv2a = prompt(" The force is strong with you " + person + "!\n" + "Do you consider yourself royalty?\n" + "yes/no")
+    if (lv2a == "yes") {
+
+      var lv3a = prompt(person + "," + " Do you remember your Mother?\n" + "yes/no")
+      if (lv3a == "yes") {
+          confirm("Don't use Jedi mindtricks on me?\n You are not " + person + " , you are Princess Leia!")
+          } else {
+//this if else statement is no registering false
+          confirm("I'm sorry " + person + " , you must be lost!")
+          }
+
+      } else {
+      var lv3b = prompt(person + "," + " did you grow up on Tatooine?\n" + "yes/no")
+      if (lv3b == "yes") {
+          confirm("Luke! I'm so glad to see you again.\n I do not understand why you thought your name was " + person + ".")
+          } else {
+          confirm("I'm sorry " + person + " , you must be lost!")
+          }
+      }
+
+  } else {
+    var user = prompt(person + ", which ship do you prefer?\n" + "millennium falcon\ntie fighter\nstar destroyer\nx-wing\n" + "Enter answer here:").toLowerCase()
+    while (user !== "millennium falcon" && user !== "tie fighter" && user !== "star destroyer" && user !== "x-wing") {
+    var user = prompt(person + ", which ship do you prefer?\n" + "millennium falcon\ntie fighter\nstar destroyer\nx-wing\n" + "Please choose one and enter answer here:").toLowerCase()
+    }
+    if (user == "millennium falcon" || "x-wing") {}
+
+    }
+}
