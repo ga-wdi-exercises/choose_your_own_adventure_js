914a4bff0ceaee9f706e2a2cf1147ebe2a867790
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-07 19:06:51 -0500

    add switch statement

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,16 @@
-// put code here!
+var name = prompt('Enter Name') // prompt user to enter name
+console.log(`Greetings ${name} welcome to the game`) // return greeting with name
+
+choice1 = prompt(`press 1 for dragons, 2 for unicorns or 3 for really much bigger dragons`) // give user first choice of three different options
+
+switch (choice1) {
+  case choice1 = '1':
+    console.log('you have chosen dragons')
+    break
+  case choice1 = '2':
+    console.log('you have chosen unicorns')
+    break
+
+  default: console.log('these dragons are much bigger, really')
+
+}
