e34a7c015f2b896c84804a872fad368130ecd69b
Kendall <adkins.kendall90@gmail.com>
2017-07-08 19:04:24 -0400

    complete prefactor

--- starter/script.js
+++ starter/script.js
@@ -7,7 +7,7 @@ function intro () {
   alert('Hello there, my name is Tune')
   alert('Welcome to Tune-Town. I hear you are looking to start a band...')
   confirm('is that correct?')
-  if (confirm) {
+  if (true) {
     alert('YAY, we are so excited to have some new tunes to jam out to')
     alert('or is it more of a sway?')
     var answer = prompt('sway or jam?')
@@ -16,36 +16,56 @@ function intro () {
       alert('ah, love some chill, calm tunes...')
       var bandName = prompt('What did you say the name of your band was?!?')
       alert(`${bandName}, that's such a chill name, the people here will love it`)
-      var bandMembers = prompt(`how many members are in ${bandName}`)
+      var bandMembers = prompt(`how many members are in ${bandName}?`)
       // enter switch
+      switch (true) {
+        case bandMembers < 2:
+        // code
+          alert(`we have a perfect spot at The Silo for ${bandName} to set up and play`)
+          break
+        case bandMembers >= 2 && bandMembers < 5 :
+        // code
+          alert(`We are going to show you to The Eucalyptis where ${bandName} can setup and play`)
+          break
+        case bandMembers >= 5 && bandMembers < 10:
+        // code
+          alert(`we have just the spot for ${bandName}... you'll be over at The Panini Shop`)
+          break
+        case bandMembers >= 10:
+        // code
+          alert(`wow, ${bandName} must be challenging Edward Sharpe for most members, we'll set you up at The Heights`)
+          break
+        default:
+        // code
+          alert(`hmm, I didn't quite catch that, I guess we can send you to the Panini Shop`)
+      }
     } else if (answer === 'jam') {
   // option
       alert('ah, jam awayyy!')
       var bandName = prompt('What did you say the name of your band was?!?')
       alert(`${bandName}, that's such a dope name, the people here will love it! before I send you to the venue...`)
-      var bandMembers = prompt(`how many members are in ${bandName}, if 1 - enter 't'; if 2-5 - enter 'n'; if 6-12 - enter 'p'; if 12+ enter 'r'`)
+      var bandMembers = prompt(`how many members are in ${bandName}?`)
       // enter switch
-      switch (bandMembers) {
-        case bandMembers === 't':
+      switch (true) {
+        case bandMembers < 2:
         // code
           alert(`we have a perfect spot at The Attic for ${bandName} to set up and play`)
           break
-        case bandMembers === 'n':
+        case bandMembers >= 2 && bandMembers < 5 :
         // code
           alert(`We are going to show you to The Cove where ${bandName} can setup and play`)
           break
-        case bandMembers === 'p':
+        case bandMembers >= 5 && bandMembers < 10:
         // code
           alert(`we have just the spot for ${bandName}... you'll be over at The Coupe`)
           break
-        case bandMembers === 'r':
+        case bandMembers >= 10:
         // code
           alert(`wow, ${bandName} must be challenging Edward Sharpe for most members, we'll set you up at The Highway Stage`)
           break
         default:
         // code
-          alert('*Please provide a valid entry*')
-          bandNumber
+          alert(`hmm, I didn't quite catch that, I guess we can put ${bandName} at The Coupe.`)
       }
     } else {
       alert('hmm, yes I\'m sure that is a viable way to listen to music but you won\'t find that here')
@@ -65,50 +85,6 @@ function intro () {
         alert('*sigh* come back some other time, bye!')
       }
     }
-  } else {
-    alert('I guess you should head over to the listening station and check out some of our latest arrivals')
-    // enter options
-    listeningBooth()
   }
 }
-
 intro()
-
-// Intro an annoyingly happy song
-
-// loop through the song, which is entered in an array
-// var enterCommissionerName = prompt('What\'s my name again, what\'s my name again?!')
-// Have user input favorite song
-// if less than x characters.. insult their song choice ...
-// between x & y say ok ...
-// otherwise phenomenal choice
-
-// what instrument do you want to play
-// guitar, drums, violin, dj
-  // if guitar - how many strings?
-    // if 6:
-      // mainstream band alert -- BOOO!
-    // if 7:
-      // your band is going to be so hip and cool
-    // if 9:
-      // your band is going to be awesome
-    // if 12:
-      // do you really need that many stings? who are you, hamilton leithauser?
-  // if drums - rock out
-    // who's your favorite drummer out of these three?
-      // 1 Carter Beauford, 2 Travis Barker, 3 Thom Sonny Green, 4 Other
-        // 1 - BOOM! My favorite too
-        // 2 - High School Angst, woooo!
-        // 3 - The most chill and deliberate drummer
-        // 4 - who else is as good as these?
-  // if violin
-    // most underrated instrument in our opinion
-    // classical, folk, or rock
-      // if classical
-        // if you bring that influence to the band, you're sure to be a hit
-      // if folk-rock
-        // underrated instrument in an underrated genre
-      // if rock
-        // the band will be great if you can play anywhere near as well as boyd tinsley
-  // if dj
-    // bye, no room in Tune-Town for another Tiesto -- only Zedd allowed here
