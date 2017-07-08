e07d0f7b83fb29ecb854b183170fceb72a30919b
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-08 13:11:35 -0400

    created if/else game

--- starter/script.js
+++ starter/script.js
@@ -1,19 +1,36 @@
 
 var name = prompt('What is your name?')
 
-if (name !== null) {
+if (name !== null) {{
   var roadTrip = prompt('Nice to meet you. Are you up for a road trip? (Yes/No)')
-}
-
-if (roadTrip === 'Yes') {
-  var whereTo = prompt('Ok, lets go. Philadelphia or Cleveland?')
-  if (whereTo === 'Philadelphia') {
-    var cheeseSteak = prompt('Ok. Cheesesteak wit wiz, or witout?')
-  } if (cheeseSteak === 'wit wiz') {
-    var youWin = prompt('You Win!! Enjoy your cheesesteak')
-  } else {
-    var youLose = prompt('Who would order a cheesesteak this way? Please go home. Game over.')
   }
-} else {
-  var planeTrip = prompt('Alright, you have $500 in airfare. Where to?')
-}
+    if (roadTrip === 'Yes') {{
+      var whereTo = prompt('Ok, lets go. Philadelphia or Cleveland?')}
+        if (whereTo === 'Cleveland') {{
+          var cavs = prompt('Are you a Cavs fan? (Yes/No)')
+            } if (cavs === 'Yes') {
+              var seriesOver = prompt('Sorry. Better luck next year')
+            } else if (cavs !== 'Yes'){
+              var goodChoice = prompt('Good choice. LeBron will be gone after next year anyway.')}}
+
+        else if (whereTo === 'Philadelphia') {{
+          var cheeseSteak = prompt('Ok. Cheesesteak or salad?')
+            } if (cheeseSteak === 'Cheesesteak') {
+              var youWin = prompt('You Win!! Enjoy your cheesesteak')
+              $('body').append("<img src='ches.jpg'/>")
+              $('body').append("<button>Click me!</button>")
+              $('button').css("font-size", 40)
+            } else {
+              var youLose = prompt('Rethink this. Game over.')}}
+            }
+
+  else {{
+    var busTrip = prompt('Alright, Greyhound it is. Where to? (Richmond, Durham or Baltimore?)')}
+       switch(busTrip) {
+        case 'Richmond': var richmond = prompt("Ok, hope you don't hit traffic.")
+        break;
+        case 'Durham': var durham = prompt("Ok, just don't talk to me about Duke basketball, please.")
+        break;
+        case 'Baltimore': var baltimore = prompt("Great choice. Grab a crabcake.")
+        break;}}
+  }
