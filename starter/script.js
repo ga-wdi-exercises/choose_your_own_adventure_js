2d52eeffbd56dae01bf0c60622e47d46bd077380
Alexander Kelly <alex.r.kelly492@gmail.com>
2017-07-10 08:36:35 -0400

    first 4 questions

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,22 @@
-// put code here!
+console.log('running')
+
+alert('Welcome to the Stadium')
+
+var hit = prompt('Did you get a hit? (yes/no)')
+
+if (hit == 'yes') {
+  var homeRun = prompt('Did you get a Home Run? (yes/ no)')
+
+  if(homeRun = 'yes') {
+    var onBase = parseInt(prompt('How many people on base? (0-4)'))
+      if(onBase >=0){
+      alert('Nice RBIs!')
+    }
+      else{
+      alert('Great Solo HR!')
+    }
+  }
+    else {
+    alert('Better Luck Next Time!')
+  }
+}
